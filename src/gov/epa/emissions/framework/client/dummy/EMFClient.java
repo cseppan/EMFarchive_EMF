9d4df3f86597b24c1d1f73d3c9d703ee3031abb7
netswirl <netswirl>
2005-07-13 20:34:05 +0000

    refactored ...added to dummy client package

---
+++ src/gov/epa/emissions/framework/client/dummy/EMFClient.java
@@ -0,0 +1,43 @@
+/*
+ * Created on Jun 27, 2005
+ *
+ * Eclipse Project Name: EMFClient
+ * Package: package gov.epa.emissions.framework.service.axis;
+ * File Name: EMFClient.java
+ * Author: Conrad F. D'Cruz
+ */
+package gov.epa.emissions.framework.client.dummy;
+  
+import gov.epa.emissions.framework.client.transport.EMFUserAdmin;
+import gov.epa.emissions.framework.client.transport.EMFUserAdminTransport;
+
+/**
+ * @author Conrad F. D'Cruz
+ *
+ */
+public class EMFClient {
+
+    /**
+     * 
+     */
+    public EMFClient() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        String statuscode = null;
+        
+        EMFUserAdmin emfUserAdmin = new EMFUserAdminTransport();
+        System.out.println("IN EMFCLIENT main");
+        
+        String uname = "ejones";
+        String pwd = "erin123";
+        
+        statuscode = emfUserAdmin.authenticate(uname, pwd, true);        
+        System.out.println(uname + " login status is: " + statuscode);
+        statuscode = emfUserAdmin.authenticate(uname, pwd, false);        
+        System.out.println(uname + " login status is: " + statuscode);
+
+    }
+}
