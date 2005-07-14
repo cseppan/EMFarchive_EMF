69849908fdf3686e715ba22ba8040d423f8f6b5f
rhavaldar <rhavaldar>
2005-07-14 21:16:00 +0000

    added a close button to the console

--- src/gov/epa/emissions/framework/client/UsersManagementConsole.java
+++ src/gov/epa/emissions/framework/client/UsersManagementConsole.java
@@ -2,8 +2,10 @@ package gov.epa.emissions.framework.client;
 
 import gov.epa.emissions.framework.client.transport.EMFUserAdmin;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -14,14 +16,32 @@ public class UsersManagementConsole extends JPanel {
 
     public UsersManagementConsole(EMFUserAdmin userAdmin) {
         this.userAdmin = userAdmin;
-                
         UsersManagementModel model = new UsersManagementModel(userAdmin.getUsers());
+
+        layout(model);
+    }
+
+    private void layout(UsersManagementModel model) {
         JTable table = new JTable(model);
-        
-        JScrollPane scrollPane = new JScrollPane(table);
+
+        JScrollPane scrollPane = new JScrollPane(table);        
         table.setPreferredScrollableViewportSize(new Dimension(600, 70));
+
+        JPanel buttonsPanel = createButtonsPanel();
         
-        this.add(scrollPane);
+        this.setLayout(new BorderLayout());        
+        this.add(scrollPane, BorderLayout.CENTER);
+        this.add(buttonsPanel, BorderLayout.SOUTH);
+    }
+
+    private JPanel createButtonsPanel() {        
+        JButton closeButton = new JButton("Close");//TODO: add event handler
+        
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new BorderLayout());
+        buttonsPanel.add(closeButton, BorderLayout.EAST);
+       
+        return buttonsPanel;
     }
 
 }
