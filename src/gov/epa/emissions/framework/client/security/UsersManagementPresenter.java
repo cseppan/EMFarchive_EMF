a57fa4f385ea5888f4fa03d6c8bc720f9237b2dd
rhavaldar <rhavaldar>
2005-07-19 19:23:02 +0000

    can delete user - Users Management (view/presenter)

--- src/gov/epa/emissions/framework/client/security/UsersManagementPresenter.java
+++ src/gov/epa/emissions/framework/client/security/UsersManagementPresenter.java
@@ -1,19 +1,31 @@
 package gov.epa.emissions.framework.client.security;
 
+import gov.epa.emissions.framework.client.transport.EMFUserAdmin;
+
 public class UsersManagementPresenter {
 
     private UsersManagementView view;
+    private EMFUserAdmin model;
 
     public UsersManagementPresenter(UsersManagementView view) {
         this.view = view;
     }
 
+    public UsersManagementPresenter(EMFUserAdmin model, UsersManagementView view) {
+        this.model = model;
+        this.view = view;
+    }
+
     public void notifyCloseView() {
-        this.view.close();
+        view.close();
     }
 
     public void init() {
-        this.view.setViewObserver(this);
+        view.setViewObserver(this);
+    }
+
+    public void notifyDelete(String username) {
+        model.deleteUser(username);
     }
 
 }
--- test/gov/epa/emissions/framework/client/security/UsersManagementPresenterTest.java
+++ test/gov/epa/emissions/framework/client/security/UsersManagementPresenterTest.java
@@ -1,5 +1,7 @@
 package gov.epa.emissions.framework.client.security;
 
+import gov.epa.emissions.framework.client.transport.EMFUserAdmin;
+
 import org.jmock.Mock;
 import org.jmock.MockObjectTestCase;
 
@@ -11,10 +13,18 @@ public class UsersManagementPresenterTest extends MockObjectTestCase {
         UsersManagementPresenter presenter = new UsersManagementPresenter((UsersManagementView) view.proxy());
 
         view.expects(once()).method("setViewObserver").with(eq(presenter));
-        view.expects(once()).method("closeView").withNoArguments();
+        view.expects(once()).method("close").withNoArguments();
 
         presenter.init();
         presenter.notifyCloseView();
     }
 
+    public void testShouldDeleteUserViaEMFUserAdminOnNotifyDelete() {
+        Mock emfUserAdmin = mock(EMFUserAdmin.class);
+        emfUserAdmin.expects(once()).method("deleteUser").with(eq("matts"));
+        
+        UsersManagementPresenter presenter = new UsersManagementPresenter((EMFUserAdmin)emfUserAdmin.proxy(), null);
+        
+        presenter.notifyDelete("matts");
+    }
 }
