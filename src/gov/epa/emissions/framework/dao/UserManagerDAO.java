9c0de1a76088485d07cdcae2e175dc1ae053e7cb
netswirl <netswirl>
2005-07-20 20:05:29 +0000

    added implementation for CRUD functions

--- src/gov/epa/emissions/framework/dao/UserManagerDAO.java
+++ src/gov/epa/emissions/framework/dao/UserManagerDAO.java
@@ -9,9 +9,12 @@
 package gov.epa.emissions.framework.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import gov.epa.emissions.framework.commons.User;
 
@@ -27,11 +30,16 @@ public class UserManagerDAO {
 
     DataSource ds = null;
     private static final String GET_USER_QUERY="select * from users where user_name='";
+    private static final String GET_USERS_QUERY="select * from users";
+    private static final String INSERT_USER_QUERY="INSERT INTO users (user_name,user_pass,fullname,affiliation,workphone,emailaddr,inadmingrp,acctdisabled) VALUES (?,?,?,?,?,?,?,?)";
+    private static final String UPDATE_USER_QUERY="UPDATE users SET user_name=?,user_pass=?,fullname=?,affiliation=?,workphone=?,emailaddr=?,inadmingrp=?,acctdisabled=? WHERE user_name=?";
+    private static final String DELETE_USER_QUERY="DELETE FROM users where user_name=?";
     
     /**
+     * @throws Exception
      * 
      */
-    public UserManagerDAO() {
+    public UserManagerDAO() throws Exception {
         super();
         try{
             Context ctx = new InitialContext();
@@ -40,13 +48,14 @@ public class UserManagerDAO {
             System.out.println("BEFORE: Is datasource null? " + (ds ==null));
             ds = (DataSource)ctx.lookup("java:/comp/env/jdbc/EMFDB");
             System.out.println("AFTER: Is datasource null? " + (ds ==null));
-        }catch(Exception e) {
-            e.printStackTrace();
+        }catch(Exception ex) {
+            ex.printStackTrace();
+            throw new Exception(ex);
         }   
     }//constructor
     
     
-    public User getUser(String userName){
+    public User getUser(String userName) throws Exception{
       System.out.println("in getUser for username= " + userName);
   
       User emfUser = new User();
@@ -64,10 +73,6 @@ public class UserManagerDAO {
               ResultSet rst = stmt.executeQuery(GET_USER_QUERY + userName + "'");
               System.out.println("Is result set null? " + (rst ==null));
               
-              // Loop through the result set
-//              if (rst.next()){
-//                  System.out.println("RST has records? " + (rst.next()));
-//                  emfUser = new User();
                   while( rst.next() ){
                       System.out.println("An rst record found ");
                       emfUser.setAcctDisabled(rst.getBoolean("acctdisabled"));
@@ -80,7 +85,6 @@ public class UserManagerDAO {
                       emfUser.setWorkPhone(rst.getString("workphone"));                      
                       emfUser.setDirty(false);
                     }//while                  
-//              }//rst.next
               
               // Close the result set, statement and the connection
               rst.close() ;
@@ -89,7 +93,9 @@ public class UserManagerDAO {
           }//conn not null
       }//ds not null
       }catch(SQLException ex){
-          
+          ex.printStackTrace();
+          throw new Exception(ex);
+
       }
 
       System.out.println("End of getUser with fullname= " + emfUser.getFullName());
@@ -98,6 +104,156 @@ public class UserManagerDAO {
     }//getUser
 
  
+    public List getUsers() throws Exception{
+      System.out.println("In getUsers");
+      
+      ArrayList users = new ArrayList();
+      User emfUser = null;
+      
+      try{
+          if (ds != null) {
+              Connection conn = ds.getConnection();
+              System.out.println("Is connection null? " + (conn ==null));
+
+              if(conn != null)  {
+                  Statement stmt = conn.createStatement();
+                  System.out.println("Is statement null? " + (stmt ==null));
+                  System.out.println("The query string " + GET_USERS_QUERY);
+
+                  ResultSet rst = stmt.executeQuery(GET_USERS_QUERY);
+                  System.out.println("Is result set null? " + (rst ==null));
+                  
+                      while( rst.next() ){
+                          System.out.println("An rst record found ");
+                          emfUser = new User();
+                          emfUser.setAcctDisabled(rst.getBoolean("acctdisabled"));
+                          emfUser.setAffiliation(rst.getString("affiliation"));
+                          emfUser.setEmailAddr(rst.getString("emailaddr"));
+                          emfUser.setFullName(rst.getString("fullname"));
+                          emfUser.setInAdminGroup(rst.getBoolean("inadmingrp"));
+                          emfUser.setPassword(rst.getString("user_pass"));
+                          emfUser.setUserName(rst.getString("user_name"));
+                          emfUser.setWorkPhone(rst.getString("workphone"));                      
+                          emfUser.setDirty(false);
+                         
+                          users.add(emfUser);
+                          System.out.println(emfUser.getUserName());
+                        }//while                  
+                  
+                  // Close the result set, statement and the connection
+                  rst.close() ;
+                  stmt.close() ;
+                  conn.close() ;
+              }//conn not null
+          }//ds not null
+          }catch(SQLException ex){
+              ex.printStackTrace();
+              throw new Exception(ex);
+          }
+      
+          System.out.println("End of getUsers size is " + users.size());
+      return users;
+    }
+    
+    public void deleteUser(String userName) throws Exception{
+      System.out.println("Begin delete user");    
+      try{
+          if (ds != null) {
+              Connection conn = ds.getConnection();
+              System.out.println("Is connection null? " + (conn ==null));
+
+              if(conn != null)  {
+                  PreparedStatement deleteStmt = conn.prepareStatement(DELETE_USER_QUERY);
+                  System.out.println("Is statement null? " + (deleteStmt ==null));
+                  System.out.println("The query string " + DELETE_USER_QUERY);
+
+                  deleteStmt.setString(1,userName);                  
+                  deleteStmt.executeUpdate();
+                                   
+                  // Close the result set, statement and the connection
+                  deleteStmt.close() ;
+                  conn.close() ;
+              }//conn not null
+          }//ds not null
+          }catch(SQLException ex){
+              ex.printStackTrace();
+              throw new Exception(ex);
+          }
         
-        
+      System.out.println("End delete user");    
+    }
+
+    public void updateUser (User user) throws Exception{
+        System.out.println("Begin update user");
+        try{
+            if (ds != null) {
+                Connection conn = ds.getConnection();
+                System.out.println("Is connection null? " + (conn ==null));
+
+                if(conn != null)  {
+                    PreparedStatement updateStmt = conn.prepareStatement(UPDATE_USER_QUERY);
+                    System.out.println("Is statement null? " + (updateStmt ==null));
+                    System.out.println("The query string " + UPDATE_USER_QUERY);
+
+                    updateStmt.setString(1,user.getUserName());
+                    updateStmt.setString(2,user.getPassword());
+                    updateStmt.setString(3,user.getFullName());
+                    updateStmt.setString(4,user.getAffiliation());
+                    updateStmt.setString(5,user.getWorkPhone());
+                    updateStmt.setString(6,user.getEmailAddr());
+                    updateStmt.setBoolean(7,user.isInAdminGroup());
+                    updateStmt.setBoolean(8,user.isAcctDisabled());
+                    
+                    updateStmt.executeUpdate();
+                    
+                    
+                    // Close the result set, statement and the connection
+                    updateStmt.close() ;
+                    conn.close() ;
+                }//conn not null
+            }//ds not null
+            }catch(SQLException ex){
+                ex.printStackTrace();
+                throw new Exception(ex);
+            }
+
+    System.out.println("End update user");
+}
+    
+    public void insertUser(User user) throws Exception{
+        System.out.println("Begin insert user");
+            try{
+                if (ds != null) {
+                    Connection conn = ds.getConnection();
+                    System.out.println("Is connection null? " + (conn ==null));
+
+                    if(conn != null)  {
+                        PreparedStatement insertStmt = conn.prepareStatement(INSERT_USER_QUERY);
+                        System.out.println("Is statement null? " + (insertStmt ==null));
+                        System.out.println("The query string " + INSERT_USER_QUERY);
+
+                        insertStmt.setString(1,user.getUserName());
+                        insertStmt.setString(2,user.getPassword());
+                        insertStmt.setString(3,user.getFullName());
+                        insertStmt.setString(4,user.getAffiliation());
+                        insertStmt.setString(5,user.getWorkPhone());
+                        insertStmt.setString(6,user.getEmailAddr());
+                        insertStmt.setBoolean(7,user.isInAdminGroup());
+                        insertStmt.setBoolean(8,user.isAcctDisabled());
+                        
+                        insertStmt.executeUpdate();
+                        
+                        
+                        // Close the result set, statement and the connection
+                        insertStmt.close() ;
+                        conn.close() ;
+                    }//conn not null
+                }//ds not null
+                }catch(SQLException ex){
+                    ex.printStackTrace();
+                    throw new Exception(ex);
+                }
+
+        System.out.println("End insert user");
+    }
 }
