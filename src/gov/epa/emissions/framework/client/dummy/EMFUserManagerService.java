4b8a500a11761ecc15c6ff6ed70e0e1fa6f028fe
netswirl <netswirl>
2005-07-20 20:03:38 +0000

    using WSDL2Java generated stubs

---
+++ src/gov/epa/emissions/framework/client/dummy/EMFUserManagerService.java
@@ -0,0 +1,16 @@
+/**
+ * EMFUserManagerService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2.1 Jun 14, 2005 (09:15:57 EDT) WSDL2Java emitter.
+ */
+
+package gov.epa.emissions.framework.client.dummy;
+
+public interface EMFUserManagerService extends javax.xml.rpc.Service {
+    public java.lang.String getEMFUserManagerServiceAddress();
+
+    public EMFUserManager getEMFUserManagerService() throws javax.xml.rpc.ServiceException;
+
+    public EMFUserManager getEMFUserManagerService(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
---
+++ src/gov/epa/emissions/framework/client/dummy/EMFWSDLClient.java
@@ -0,0 +1,57 @@
+/*
+ * Created on Jul 18, 2005
+ *
+ * Eclipse Project Name: EMF
+ * Package: package gov.epa.emissions.framework.client.dummy;
+ * File Name: EMFWSDLClient.java
+ * Author: Conrad F. D'Cruz
+ */
+package gov.epa.emissions.framework.client.dummy;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+
+import org.apache.axis.AxisFault;
+import org.apache.axis.client.Service;
+
+/**
+ * @author Conrad F. D'Cruz
+ *
+ */
+public class EMFWSDLClient {
+    private static String endpoint = 
+        "http://ben.cep.unc.edu:8080/emf/services/EMFUserManagerService";
+
+    /**
+     * 
+     */
+    public EMFWSDLClient() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        Service  service = new Service();
+        URL url;
+        try {
+            url = new URL(endpoint);
+            EMFUserManagerServiceSoapBindingStub emfstub = new EMFUserManagerServiceSoapBindingStub(url,service);
+            Object[] users = emfstub.getUsers();
+            
+            System.out.println(users.length);
+            
+            //User user = emfstub.getUser("ejones");
+            //System.out.println(user.getFullName());
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (AxisFault e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
