88d5fee245bf4ac8fe21165c37ae5ca5bea9fe14
rhavaldar <rhavaldar>
2005-07-21 21:06:42 +0000

    'creating user' - in progress

---
+++ test/gov/epa/emissions/framework/client/login/CreateUserPresenterTest.java
@@ -0,0 +1,36 @@
+package gov.epa.emissions.framework.client.login;
+
+import gov.epa.emissions.framework.EmfException;
+import gov.epa.emissions.framework.commons.EMFUserAdmin;
+import gov.epa.emissions.framework.commons.User;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+
+public class CreateUserPresenterTest extends MockObjectTestCase {
+    
+    public void testShouldCreateUserWithAllFieldsPopulatedOnCreate() throws EmfException {
+        User user = new User();
+        user.setFullName("Jim Jom");
+        user.setAffiliation("Jim Inc.");
+        user.setWorkPhone("123");
+        user.setEmailAddr("jim@jom.com");
+        
+        user.setUserName("jimmy");
+        user.setPassword("jimjam");
+        
+        Mock userAdmin = mock(EMFUserAdmin.class);
+        userAdmin.expects(once()).method("createUser").with(eq(user));
+        userAdmin.expects(once()).method("authenticate").with(eq("jimmy"), eq("jimjam"), eq(false)).will(returnValue(null));
+        
+        CreateUserPresenter presenter = new CreateUserPresenter((EMFUserAdmin) userAdmin.proxy());
+        
+        presenter.create(user.getFullName(),
+                         user.getAffiliation(),
+                         user.getWorkPhone(), 
+                         user.getEmailAddr(), 
+                         user.getUserName(),
+                         user.getPassword(),
+                         user.getPassword());
+    }
+}
