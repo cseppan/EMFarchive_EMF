9d5390b76282113990a6a46e744d3b88e6c30c1a
netswirl <netswirl>
2005-08-04 20:44:24 +0000

    New Data Service interface

---
+++ src/gov/epa/emissions/framework/commons/EMFData.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Jul 28, 2005
+ *
+ * Eclipse Project Name: EMF
+ * Package: package gov.epa.emissions.framework.commons;
+ * File Name: EMFData.java
+ * Author: Conrad F. D'Cruz
+ */
+package gov.epa.emissions.framework.commons;
+
+import gov.epa.emissions.framework.EmfException;
+
+/**
+ * @author Conrad F. D'Cruz
+ *
+ */
+public interface EMFData {
+
+    public void startImport(String fileName, String fileType) throws EmfException;
+}
