7074ca344d7cf8b58f4f567d7ef315ef49d98233
netswirl <netswirl>
2005-08-18 01:49:26 +0000

    completed the definition of the DatasetType

--- src/gov/epa/emissions/framework/DatasetType.java
+++ src/gov/epa/emissions/framework/DatasetType.java
@@ -1,9 +1,23 @@
 package gov.epa.emissions.framework;
 
-public class DatasetType {
+import java.io.Serializable;
 
+public class DatasetType implements Serializable{
+    
+	/**
+	 * Generated serialVersionUID
+	 */
+	private static final long serialVersionUID = 4694789007596096797L;
+	private long id;
     private String name;
-
+    private String description;
+    private int minfiles;
+    private int maxfiles;
+    
+    public DatasetType(){
+    	super();
+    }
+    
     public DatasetType(String name) {
         this.name = name;
     }
@@ -11,5 +25,41 @@ public class DatasetType {
     public String getName() {
         return name;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getMaxfiles() {
+		return maxfiles;
+	}
+
+	public void setMaxfiles(int maxfiles) {
+		this.maxfiles = maxfiles;
+	}
+
+	public int getMinfiles() {
+		return minfiles;
+	}
+
+	public void setMinfiles(int minfiles) {
+		this.minfiles = minfiles;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
