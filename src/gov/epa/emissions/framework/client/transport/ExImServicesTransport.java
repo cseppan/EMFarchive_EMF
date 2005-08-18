0f29d1ea1c8a35546377b63184be8f3e485e4f88
netswirl <netswirl>
2005-08-18 02:24:44 +0000

    added methods for getDatasetTypes

--- src/gov/epa/emissions/framework/client/transport/ExImServicesTransport.java
+++ src/gov/epa/emissions/framework/client/transport/ExImServicesTransport.java
@@ -11,6 +11,7 @@ package gov.epa.emissions.framework.client.transport;
 import gov.epa.emissions.framework.DatasetType;
 import gov.epa.emissions.framework.EmfException;
 import gov.epa.emissions.framework.commons.ExImServices;
+import gov.epa.emissions.framework.commons.Status;
 import gov.epa.emissions.framework.commons.User;
 
 import java.net.MalformedURLException;
@@ -72,7 +73,7 @@ public class ExImServicesTransport implements ExImServices {
             call = (Call) service.createCall();
             call.setTargetEndpointAddress( new java.net.URL(endpoint) );
             
-            QName qname3 = new QName("urn:EMFDataService", "startImport");
+            QName qname3 = new QName("urn:ExImServices", "startImport");
             
             call.setOperationName(qname3);
             
@@ -110,7 +111,45 @@ public class ExImServicesTransport implements ExImServices {
      * @see gov.epa.emissions.framework.commons.ExImServices#startImport(gov.epa.emissions.framework.commons.User, java.lang.String, gov.epa.emissions.framework.commons.DatasetType)
      */
     public void startImport(User user, String fileName, DatasetType datasetType) throws EmfException {
-        // TODO Auto-generated method stub
+        Service  service = new Service();
+        Call     call;
+
+//        try {
+//            call = (Call) service.createCall();
+//            call.setTargetEndpointAddress( new java.net.URL(endpoint) );
+//            
+//            QName qname3 = new QName("urn:ExImServices", "startImport");
+//            
+//            call.setOperationName(qname3);
+//            
+//            call.addParameter("username",
+//                    org.apache.axis.Constants.XSD_STRING,
+//                    javax.xml.rpc.ParameterMode.IN);
+//
+//            call.addParameter("filename",
+//                            org.apache.axis.Constants.XSD_STRING,
+//                            javax.xml.rpc.ParameterMode.IN);
+//		    call.addParameter("filetype",
+//                            org.apache.axis.Constants.XSD_STRING,
+//                            javax.xml.rpc.ParameterMode.IN);
+//		          
+//            call.setReturnType(org.apache.axis.Constants.XSD_ANY);
+//            
+//            Object obj = call.invoke( new Object[] {userName, fileName, fileType} );
+//            
+//        } catch (ServiceException e) {
+//            System.out.println("Error invoking the service");
+//            e.printStackTrace();
+//        } catch (MalformedURLException e) {
+//            System.out.println("Error in format of URL string");
+//            e.printStackTrace();
+//        } catch (AxisFault fault){
+//            throw new EmfException(extractMessage(fault.getMessage()));           
+//        }catch (RemoteException e) {
+//            System.out.println("Error communicating with WS end point");
+//            e.printStackTrace();
+//        }
+        
         
     }
 
@@ -118,8 +157,56 @@ public class ExImServicesTransport implements ExImServices {
      * @see gov.epa.emissions.framework.commons.ExImServices#getDatasetTypes()
      */
     public DatasetType[] getDatasetTypes() {
-        // TODO Auto-generated method stub
-        return null;
+    	DatasetType[] datasettypes = null;
+   
+    	// Call the ExImServices endpoint and acquire the array of all dataset types
+    	//defined in the system
+//        Status[] allStats = null;;
+//        
+//        Service  service = new Service();
+//        Call     call;
+//
+//        try {
+//            call = (Call) service.createCall();
+//            call.setTargetEndpointAddress( new java.net.URL(endpoint) );
+//            
+//            QName qname1 = new QName("urn:gov.epa.emf.StatusServices","ns1:DatasetType");
+//            QName qname2 = new QName("urn:gov.epa.emf.StatusServices","ns1:DatasetTypes");
+//            QName qname3 = new QName("urn:gov.epa.emf.StatusServices", "getDatasetTypes");
+//            
+//            call.setOperationName(qname3);
+//            
+//            Class cls1 = gov.epa.emissions.framework.commons.Status.class;
+//            Class cls2 = gov.epa.emissions.framework.commons.Status[].class;
+//	          call.registerTypeMapping(cls1, qname1,
+//					  new org.apache.axis.encoding.ser.BeanSerializerFactory(cls1, qname1),        
+//					  new org.apache.axis.encoding.ser.BeanDeserializerFactory(cls1, qname1));        			  
+//		          call.registerTypeMapping(cls2, qname2,
+//						  new org.apache.axis.encoding.ser.ArraySerializerFactory(cls2, qname2),        
+//						  new org.apache.axis.encoding.ser.ArrayDeserializerFactory(qname2));        			  
+//		            call.addParameter("uname",
+//                            org.apache.axis.Constants.XSD_STRING,
+//                            javax.xml.rpc.ParameterMode.IN);
+//		          
+//            call.setReturnType(qname2);
+//            
+//            Object obj = call.invoke( new Object[] {userName} );
+//                            
+//            allStats = (Status[])obj;
+//            
+//        } catch (ServiceException e) {
+//            System.out.println("Error invoking the service");
+//            e.printStackTrace();
+//        } catch (MalformedURLException e) {
+//            System.out.println("Error in format of URL string");
+//            e.printStackTrace();
+//        } catch (AxisFault fault){
+//            throw new EmfException(extractMessage(fault.getMessage()));           
+//        }catch (RemoteException e) {
+//            System.out.println("Error communicating with WS end point");
+//            e.printStackTrace();
+//        }
+    	return datasettypes;
     }
    
 
