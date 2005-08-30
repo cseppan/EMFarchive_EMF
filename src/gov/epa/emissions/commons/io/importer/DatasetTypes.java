179a08ac076a7aa584c845b73413458b758a51bb
rhavaldar <rhavaldar>
2005-08-30 18:54:12 +0000

    Refactoring DatasetTypes and TableTypes to enable the ORL Exporter to automatically identify the Table Types associated w/ a DatasetType

--- src/gov/epa/emissions/commons/io/importer/DatasetTypes.java
+++ src/gov/epa/emissions/commons/io/importer/DatasetTypes.java
@@ -1,32 +1,9 @@
 package gov.epa.emissions.commons.io.importer;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-/**
- * @version $Id: DatasetTypes.java,v 1.5 2005/08/25 21:00:09 rhavaldar Exp $
- * 
- */
 public final class DatasetTypes {
-    /**
-     * Enumeration elements are constructed once upon class loading. Order of
-     * appearance here determines index value.
-     */
-    public static final String NIF_AREA = "NIF3 Nonpoint Inventory";
-
-    public static final String NIF_POINT = "NIF3 Point Inventory";
-
-    public static final String NIF_MOBILE_EMISSIONS = "NIF3 Mobile Onroad Emissions Inventory";
-
-    public static final String IDA_AREA = "IDA Area Inventory";
-
-    public static final String IDA_POINT = "IDA Point Inventory";
-
-    public static final String IDA_MOBILE_EMISSIONS = "IDA Mobile Emissions Inventory";
-
-    public static final String IDA_MOBILE_ACTIVITY = "IDA Mobile Activity Inventory";
 
     public static final String ORL_AREA_NONPOINT_TOXICS = "ORL Nonpoint Inventory";
 
@@ -38,75 +15,15 @@ public final class DatasetTypes {
 
     public static final String REFERENCE = "Reference";
 
-    /** List of all NAMES in enumeration. */
-    public static final List NAMES;
-
-    /** Map of all table names for a given Dataset type. */
-    private static final Map TABLE_NAMES;
-
-    private static final Map SUMMARY_TABLE_TYPE;
-
-    /**
-     * descriptive column names for each dataset type
-     */
-    private static String[] NIF_AREA_COLUMN_NAMES = { "State", "FIPS", "SCC", "MACT", "SIC", "NAICS" };
-
-    private static String[] NIF_POINT_COLUMN_NAMES = { "State", "FIPS", "Facility", "Unit", "Process", "Point", "SCC",
-            "MACT", "SIC", "NAICS" };
-
-    private static String[] NIF_MOBILE_EMISSIONS_COLUMN_NAMES = { "State", "FIPS" };
-
-    private static final Map DESCRIPTIVE_COLUMN_NAMES;
+    private static final Map DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP;
 
     static {
-        NAMES = new ArrayList();
-        NAMES.add(NIF_AREA);
-        NAMES.add(NIF_POINT);
-        NAMES.add(NIF_MOBILE_EMISSIONS);
-        // TODO keithlee - uncomment to add as option in GUI
-        // NAMES.add(NIF_MOBILE_ACTIVITY);
-        NAMES.add(IDA_AREA);
-        NAMES.add(IDA_POINT);
-        NAMES.add(IDA_MOBILE_EMISSIONS);
-        // TODO keithlee - uncomment to add as option in GUI
-        // NAMES.add(IDA_MOBILE_ACTIVITY);
-        // NAMES.add(REFERENCE);
-        NAMES.add(ORL_AREA_NONPOINT_TOXICS);
-        NAMES.add(ORL_AREA_NONROAD_TOXICS);
-        NAMES.add(ORL_ON_ROAD_MOBILE_TOXICS);
-        NAMES.add(ORL_POINT_TOXICS);
-
-        SUMMARY_TABLE_TYPE = new HashMap();
-        SUMMARY_TABLE_TYPE.put(NIF_AREA, TableTypes.NIF_AREA_SUMMARY);
-        SUMMARY_TABLE_TYPE.put(NIF_MOBILE_EMISSIONS, TableTypes.NIF_MOBILE_EMISSIONS_SUMMARY);
-        SUMMARY_TABLE_TYPE.put(NIF_POINT, TableTypes.NIF_POINT_SUMMARY);
-        SUMMARY_TABLE_TYPE.put(IDA_AREA, TableTypes.IDA_AREA);
-        SUMMARY_TABLE_TYPE.put(IDA_POINT, TableTypes.IDA_POINT);
-        SUMMARY_TABLE_TYPE.put(IDA_MOBILE_EMISSIONS, TableTypes.IDA_MOBILE_EMISSIONS);
-        SUMMARY_TABLE_TYPE.put(IDA_MOBILE_ACTIVITY, TableTypes.IDA_MOBILE_ACTIVITY);
-        SUMMARY_TABLE_TYPE.put(ORL_AREA_NONPOINT_TOXICS, TableTypes.ORL_AREA_NONPOINT_TOXICS_SUMMARY);
-        SUMMARY_TABLE_TYPE.put(ORL_AREA_NONROAD_TOXICS, TableTypes.ORL_AREA_NONROAD_TOXICS_SUMMARY);
-        SUMMARY_TABLE_TYPE.put(ORL_ON_ROAD_MOBILE_TOXICS, TableTypes.ORL_MOBILE_TOXICS_SUMMARY);
-        SUMMARY_TABLE_TYPE.put(ORL_POINT_TOXICS, TableTypes.ORL_POINT_TOXICS_SUMMARY);
-
-        TABLE_NAMES = new HashMap();
-        TABLE_NAMES.put(NIF_AREA, TableTypes.getNamesArray(NIF_AREA));
-        TABLE_NAMES.put(NIF_POINT, TableTypes.getNamesArray(NIF_POINT));
-        TABLE_NAMES.put(NIF_MOBILE_EMISSIONS, TableTypes.getNamesArray(NIF_MOBILE_EMISSIONS));
-        TABLE_NAMES.put(IDA_AREA, TableTypes.getNamesArray(IDA_AREA));
-        TABLE_NAMES.put(IDA_POINT, TableTypes.getNamesArray(IDA_POINT));
-        TABLE_NAMES.put(IDA_MOBILE_EMISSIONS, TableTypes.getNamesArray(IDA_MOBILE_EMISSIONS));
-        TABLE_NAMES.put(IDA_MOBILE_ACTIVITY, TableTypes.getNamesArray(IDA_MOBILE_ACTIVITY));
-        TABLE_NAMES.put(REFERENCE, TableTypes.getNamesArray(REFERENCE));
-        TABLE_NAMES.put(ORL_AREA_NONPOINT_TOXICS, TableTypes.getNamesArray(ORL_AREA_NONPOINT_TOXICS));
-        TABLE_NAMES.put(ORL_AREA_NONROAD_TOXICS, TableTypes.getNamesArray(ORL_AREA_NONROAD_TOXICS));
-        TABLE_NAMES.put(ORL_ON_ROAD_MOBILE_TOXICS, TableTypes.getNamesArray(ORL_ON_ROAD_MOBILE_TOXICS));
-        TABLE_NAMES.put(ORL_POINT_TOXICS, TableTypes.getNamesArray(ORL_POINT_TOXICS));
-
-        DESCRIPTIVE_COLUMN_NAMES = new HashMap();
-        DESCRIPTIVE_COLUMN_NAMES.put(NIF_AREA, NIF_AREA_COLUMN_NAMES);
-        DESCRIPTIVE_COLUMN_NAMES.put(NIF_POINT, NIF_POINT_COLUMN_NAMES);
-        DESCRIPTIVE_COLUMN_NAMES.put(NIF_MOBILE_EMISSIONS, NIF_MOBILE_EMISSIONS_COLUMN_NAMES);
+        DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP = new HashMap();
+        DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP.put(ORL_AREA_NONPOINT_TOXICS,
+                TableTypes.ORL_AREA_NONPOINT_TOXICS_SUMMARY);
+        DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP.put(ORL_AREA_NONROAD_TOXICS, TableTypes.ORL_AREA_NONROAD_TOXICS_SUMMARY);
+        DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP.put(ORL_ON_ROAD_MOBILE_TOXICS, TableTypes.ORL_MOBILE_TOXICS_SUMMARY);
+        DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP.put(ORL_POINT_TOXICS, TableTypes.ORL_POINT_TOXICS_SUMMARY);
     }
 
     public static boolean isORLDataset(String datasetType) {
@@ -116,18 +33,12 @@ public final class DatasetTypes {
         return false;
     }
 
-    private DatasetTypes() {
-    }
-
-    /**
-     * Get the valid table types for a given dataset type
-     */
     public static String[] getTableTypes(final String datasetType) {
-        return (String[]) TABLE_NAMES.get(datasetType);
+        return TableTypes.getTableTypes(datasetType);
     }
 
     public static String getSummaryTableType(final String datasetType) {
-        return (String) SUMMARY_TABLE_TYPE.get(datasetType);
+        return (String) DATASET_TYPE_TO_SUMMARY_TABLE_TYPE_MAP.get(datasetType);
     }
 
 }
--- src/gov/epa/emissions/commons/io/importer/TableTypes.java
+++ src/gov/epa/emissions/commons/io/importer/TableTypes.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Table type String constants
  * 
  * @author Craig Mattocks, Keith Lee
- * @version $Id: TableTypes.java,v 1.4 2005/08/25 21:00:09 rhavaldar Exp $
+ * @version $Id: TableTypes.java,v 1.5 2005/08/30 18:54:12 rhavaldar Exp $
  */
 public final class TableTypes {
     public static final String IDA_AREA = "IDA Area Source Emissions";
@@ -96,77 +96,31 @@ public final class TableTypes {
 
     public static final String ORL_POINT_TOXICS_SUMMARY = "ORL Point Toxics Summary";
 
-    /** List of all NAMES in enumeration. */
-    // public static final java.util.List NAMES =
-    // getAllNames(IDAMobileTableTypes.class);
-    /** List of all objects in enumeration. */
-    // public static final java.util.List VALUES =
-    // getInstances(NIFPointTableTypes.class);
-    /** */
-    private static final Map DATASET_TYPE_TO_NAMES_ARRAY_MAP;
-
-    private static final Map EMISSION_TABLE_NAMES;
+    private static final Map DATASET_TYPE_TO_TABLE_TYPES_MAP;
 
     private static final Map DATASET_TYPE_TO_MAP_OF_FILENAME_TO_TABLE_TYPE_MAP;
 
     static {
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP = new HashMap();
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.IDA_AREA, new String[] { IDA_AREA });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.IDA_MOBILE_EMISSIONS, new String[] { IDA_MOBILE_EMISSIONS });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.IDA_MOBILE_ACTIVITY, new String[] { IDA_MOBILE_ACTIVITY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.IDA_POINT, new String[] { IDA_POINT });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.NIF_AREA, new String[] { NIF_AREA_CE, NIF_AREA_EM,
-                NIF_AREA_EP, NIF_AREA_PE, NIF_AREA_SUMMARY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.NIF_POINT, new String[] { NIF_POINT_CE, NIF_POINT_EM,
-                NIF_POINT_EP, NIF_POINT_ER, NIF_POINT_EU, NIF_POINT_PE, NIF_POINT_SI, NIF_POINT_SUMMARY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.NIF_MOBILE_EMISSIONS, new String[] { NIF_MOBILE_EMISSIONS_EM,
-                NIF_MOBILE_EMISSIONS_PE, NIF_MOBILE_EMISSIONS_SUMMARY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.REFERENCE, new String[] { REF_CONTROL_DEVICE_CODES,
-                REF_CONVERSION_FACTORS, /* REF_COUNTRIES, */
-                REF_EMISSION_TYPES, REF_EMISSION_UNITS_CODES, REF_FIPS, REF_MACT_CODES, REF_MATERIAL_CODES,
-                REF_NAICS_CODES, REF_POLLUTANT_CODES, /* REF_POLLUTANTS, */REF_SCC,
-                /* REF_SECTORS, */REF_SIC_CODES, /* REF_STATES, */REF_TIME_ZONES, REF_TRIBAL_CODES });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.ORL_AREA_NONPOINT_TOXICS, new String[] {
+        DATASET_TYPE_TO_TABLE_TYPES_MAP = new HashMap();
+        DATASET_TYPE_TO_TABLE_TYPES_MAP.put(DatasetTypes.REFERENCE, new String[] { REF_CONTROL_DEVICE_CODES,
+                REF_CONVERSION_FACTORS, REF_EMISSION_TYPES, REF_EMISSION_UNITS_CODES, REF_FIPS, REF_MACT_CODES,
+                REF_MATERIAL_CODES, REF_NAICS_CODES, REF_POLLUTANT_CODES, REF_SCC, REF_SIC_CODES, REF_TIME_ZONES,
+                REF_TRIBAL_CODES });
+                
+        DATASET_TYPE_TO_TABLE_TYPES_MAP.put(DatasetTypes.ORL_AREA_NONPOINT_TOXICS, new String[] {
                 ORL_AREA_NONPOINT_TOXICS, ORL_AREA_NONPOINT_TOXICS_SUMMARY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.ORL_AREA_NONROAD_TOXICS, new String[] {
+        DATASET_TYPE_TO_TABLE_TYPES_MAP.put(DatasetTypes.ORL_AREA_NONROAD_TOXICS, new String[] {
                 ORL_AREA_NONROAD_TOXICS, ORL_AREA_NONROAD_TOXICS_SUMMARY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.ORL_ON_ROAD_MOBILE_TOXICS, new String[] { ORL_MOBILE_TOXICS,
+        DATASET_TYPE_TO_TABLE_TYPES_MAP.put(DatasetTypes.ORL_ON_ROAD_MOBILE_TOXICS, new String[] { ORL_MOBILE_TOXICS,
                 ORL_MOBILE_TOXICS_SUMMARY });
-        DATASET_TYPE_TO_NAMES_ARRAY_MAP.put(DatasetTypes.ORL_POINT_TOXICS, new String[] { ORL_POINT_TOXICS,
+        DATASET_TYPE_TO_TABLE_TYPES_MAP.put(DatasetTypes.ORL_POINT_TOXICS, new String[] { ORL_POINT_TOXICS,
                 ORL_POINT_TOXICS_SUMMARY });
-
-        EMISSION_TABLE_NAMES = new HashMap();
-        EMISSION_TABLE_NAMES.put(DatasetTypes.NIF_AREA, NIF_AREA_EM);
-        EMISSION_TABLE_NAMES.put(DatasetTypes.NIF_POINT, NIF_POINT_EM);
 
         DATASET_TYPE_TO_MAP_OF_FILENAME_TO_TABLE_TYPE_MAP = new HashMap();
         Map filenameToTableTypeMap = new HashMap();
-        filenameToTableTypeMap.put("area_ce", NIF_AREA_CE);
-        filenameToTableTypeMap.put("area_em", NIF_AREA_EM);
-        filenameToTableTypeMap.put("area_ep", NIF_AREA_EP);
-        filenameToTableTypeMap.put("area_pe", NIF_AREA_PE);
-        DATASET_TYPE_TO_MAP_OF_FILENAME_TO_TABLE_TYPE_MAP.put(DatasetTypes.NIF_AREA, filenameToTableTypeMap);
-        filenameToTableTypeMap = new HashMap();
-        filenameToTableTypeMap.put("point_ce", NIF_POINT_CE);
-        filenameToTableTypeMap.put("point_em", NIF_POINT_EM);
-        filenameToTableTypeMap.put("point_ep", NIF_POINT_EP);
-        filenameToTableTypeMap.put("point_er", NIF_POINT_ER);
-        filenameToTableTypeMap.put("point_eu", NIF_POINT_EU);
-        filenameToTableTypeMap.put("point_pe", NIF_POINT_PE);
-        filenameToTableTypeMap.put("point_si", NIF_POINT_SI);
-        DATASET_TYPE_TO_MAP_OF_FILENAME_TO_TABLE_TYPE_MAP.put(DatasetTypes.NIF_POINT, filenameToTableTypeMap);
-        filenameToTableTypeMap = new HashMap();
-        // TODO keithlee - make sure this is what NIF mobile onroad files should
-        // have in their name, otherwise, lookup via getTableType() won't work
-        // (used in FormatttedImporter checkFiles())
-        filenameToTableTypeMap.put("onroad_em", NIF_MOBILE_EMISSIONS_EM);
-        filenameToTableTypeMap.put("onroad_pe", NIF_MOBILE_EMISSIONS_PE);
-        DATASET_TYPE_TO_MAP_OF_FILENAME_TO_TABLE_TYPE_MAP
-                .put(DatasetTypes.NIF_MOBILE_EMISSIONS, filenameToTableTypeMap);
         filenameToTableTypeMap = new HashMap();
         filenameToTableTypeMap.put("control_device_codes", REF_CONTROL_DEVICE_CODES);
         filenameToTableTypeMap.put("conversion_factors", REF_CONVERSION_FACTORS);
-        // filenameToTableTypeMap.put("countries", REF_COUNTRIES);
         filenameToTableTypeMap.put("emission_types", REF_EMISSION_TYPES);
         filenameToTableTypeMap.put("emission_units_codes", REF_EMISSION_UNITS_CODES);
         filenameToTableTypeMap.put("fips", REF_FIPS);
@@ -174,30 +128,11 @@ public final class TableTypes {
         filenameToTableTypeMap.put("material_codes", REF_MATERIAL_CODES);
         filenameToTableTypeMap.put("naics_codes", REF_NAICS_CODES);
         filenameToTableTypeMap.put("pollutant_codes", REF_POLLUTANT_CODES);
-        // filenameToTableTypeMap.put("pollutants", REF_POLLUTANTS);
         filenameToTableTypeMap.put("scc", REF_SCC);
-        // filenameToTableTypeMap.put("sectors", REF_SECTORS);
         filenameToTableTypeMap.put("sic_codes", REF_SIC_CODES);
-        // filenameToTableTypeMap.put("states", REF_STATES);
         filenameToTableTypeMap.put("time_zones", REF_TIME_ZONES);
         filenameToTableTypeMap.put("tribal_codes", REF_TRIBAL_CODES);
         DATASET_TYPE_TO_MAP_OF_FILENAME_TO_TABLE_TYPE_MAP.put(DatasetTypes.REFERENCE, filenameToTableTypeMap);
-    }
-
-    public static final String[] DEFAULT_NIF_AREA_NAMES = { "nei_area_ce", "nei_area_em", "nei_area_ep", "nei_area_pe",
-            "nei_area_summary" };
-
-    public static final String[] DEFAULT_NIF_POINT_NAMES = { "nei_point_ce", "nei_point_em", "nei_point_ep",
-            "nei_point_er", "nei_point_eu", "nei_point_pe", "nei_point_si", "nei_point_summary" };
-
-    public static final String[] DEFAULT_NIF_MOBILE_EMISSIONS_NAMES = { "nei_onroad_em", "nei_onroad_pe",
-            "nei_onroad_summary" };
-
-    /**
-     * Private constructor prevents construction outside of this class.
-     */
-    private TableTypes() {
-        /** DO NOTHING * */
     }
 
     /**
@@ -205,8 +140,8 @@ public final class TableTypes {
      * 
      * @return An unmodifiable String[] Array of the names.
      */
-    public static final String[] getNamesArray(String datasetType) {
-        return (String[]) DATASET_TYPE_TO_NAMES_ARRAY_MAP.get(datasetType);
+    public static final String[] getTableTypes(String datasetType) {
+        return (String[]) DATASET_TYPE_TO_TABLE_TYPES_MAP.get(datasetType);
     }
 
     // some helper methods
