f9be1d538db507a4a6f4accaafd75d203b007d2e
netswirl <netswirl>
2005-09-12 19:16:29 +0000

    Utility class that provides password encryption service.

---
+++ src/gov/epa/emissions/framework/client/admin/PasswordService.java
@@ -0,0 +1,66 @@
+/*
+ * Creation on Sep 12, 2005
+ * Eclipse Project Name: EMF
+ * File Name: PasswordService.java
+ * Author: Conrad F. D'Cruz
+ */
+/**
+ * 
+ */
+
+package gov.epa.emissions.framework.client.admin;
+
+import gov.epa.emissions.framework.EmfException;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import sun.misc.BASE64Encoder;
+
+/**
+ * @author Conrad F. D'Cruz
+ *
+ */
+public class PasswordService {
+
+	/**
+	 * 
+	 */
+	public PasswordService() {
+		super();
+	}
+
+	/**
+	 * This method encrypts the plain text password and returns a
+	 * string.
+	 * 
+	 * @param textPassword
+	 * @return encrypted password
+	 * @throws EmfException
+	 */
+	  public String encrypt(String textPassword) throws EmfException
+	  {
+	    MessageDigest md = null;
+	    try
+	    {
+	      md = MessageDigest.getInstance("SHA");
+	    }
+	    catch(NoSuchAlgorithmException e)
+	    {
+	      throw new EmfException(e.getMessage());
+	    }
+	    try
+	    {
+	      md.update(textPassword.getBytes("UTF-8"));
+	    }
+	    catch(UnsupportedEncodingException e)
+	    {
+	      throw new EmfException(e.getMessage());
+	    }
+	    byte raw[] = md.digest();
+	    String hash = (new BASE64Encoder()).encode(raw);
+	    return hash;
+	  }
+
+}//PasswordService
