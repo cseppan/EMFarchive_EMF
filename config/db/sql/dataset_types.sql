e95ae157134adf70cffa822f7b3c7b9dd9774f6d
netswirl <netswirl>
2005-10-06 12:30:16 +0000

    support for shape and external files

--- config/db/sql/dataset_types.sql
+++ config/db/sql/dataset_types.sql
@@ -3,3 +3,6 @@ insert into emf.datasettypes values(1,'ORL Nonpoint Inventory','ORL Nonpoint Inv
 insert into emf.datasettypes values(2,'ORL Nonroad Inventory','ORL Nonroad Inventory', '1', '1', 4, 8);
 insert into emf.datasettypes values(3,'ORL Onroad Inventory','ORL Onroad Inventory', '1', '1', 4, 5);
 insert into emf.datasettypes values(4,'ORL Point Inventory','ORL Point Inventory', '1', '1', 23, 28);
+insert into emf.datasettypes values(5,'Shapefile','Shape files', '3', '-1','-1','-1');
+insert into emf.datasettypes values(6,'Externalfile','External files', '1', '-1','-1','-1');
+insert into emf.datasettypes values(7,'Meteorologyfile','Meteorology files', '1', '-1','-1','-1');
