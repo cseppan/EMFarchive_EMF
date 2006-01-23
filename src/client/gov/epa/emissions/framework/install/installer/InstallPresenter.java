33b9d7a9478f013b45e4176d699af744ab64a3c5
qunhe <qunhe>
2006-01-23 15:34:23 +0000

    Local ClientBatchfile changed, so the InstallPresenter

--- src/client/gov/epa/emissions/framework/install/installer/InstallPresenter.java
+++ src/client/gov/epa/emissions/framework/install/installer/InstallPresenter.java
@@ -1,14 +1,15 @@
 package gov.epa.emissions.framework.install.installer;
 
-import gov.epa.emissions.commons.io.importer.ImporterException;
-
 import java.awt.Cursor;
-import java.io.IOException;
 
 public class InstallPresenter {
     private InstallView view;
-
+    
     private Download model;
+
+//    public InstallPresenter(Download model) {
+//        this.model = model;
+//    }
 
     public void doCancel() {
         view.close();
@@ -20,50 +21,50 @@ public class InstallPresenter {
 
         view.display();
     }
-
+    
     public void startDownload(String url, String filelist, String installhome) {
         model = new Download(url, filelist, installhome);
         model.addObserver(this);
         model.start();
     }
-
+    
     public void stopDownload() {
         model.stopDownload();
+        //model.stop();
     }
-
-    public void writePreference(String website, String input, String output, String javahome, String emfhome)
-            throws Exception {
+    
+    public void writePreference(String website, String input, 
+            String output, String javahome, String emfhome) {
         Tools.writePreference(website, input, output, javahome, emfhome);
     }
-
+    
     public void setStatus(String status) {
         view.setStatus(status);
     }
-
+    
     public void setCursor(Cursor cursor) {
         view.setCursor(cursor);
     }
-
+    
     public void displayErr(String err) {
         view.displayErr(err);
     }
-
+    
     public void setFinish() {
         view.setFinish();
     }
-
-    public void createBatchFile(String filename, String preference, String emfhome, String javahome) {
+    
+    public void createBatchFile(String filename, String preference, 
+            String javahome) {
         try {
-            new ClientBatchFile(filename).create(preference, emfhome, javahome);
-        } catch (ImporterException e) {
-            view.displayErr("Creating EMF client batch file failed.");
-        } catch (IOException e) {
+            new ClientBatchFile(filename).create(preference, javahome);
+        } catch (Exception e) {
             view.displayErr("Creating EMF client batch file failed.");
         }
     }
-
+    
     public void createShortcut() {
         model.createShortcut();
     }
-
+    
 }
