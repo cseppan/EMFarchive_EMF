dc713105a4323ad32b4f727fdc63498f95950df7
parthee <parthee>
2006-01-30 17:58:37 +0000

    dataset viewer is updated to show the keywords belongs to the dataset type and the dataset itself.

--- test/unit/gov/epa/emissions/framework/client/meta/keywords/KeywordsTabPresenterTest.java
+++ test/unit/gov/epa/emissions/framework/client/meta/keywords/KeywordsTabPresenterTest.java
@@ -1,5 +1,6 @@
 package gov.epa.emissions.framework.client.meta.keywords;
 
+import gov.epa.emissions.commons.io.DatasetType;
 import gov.epa.emissions.commons.io.KeyVal;
 import gov.epa.emissions.framework.services.EmfDataset;
 
@@ -12,9 +13,11 @@ public class KeywordsTabPresenterTest extends MockObjectTestCase {
         KeyVal[] values = new KeyVal[] { new KeyVal(), new KeyVal() };
         Mock view = mock(KeywordsTabView.class);
         view.expects(once()).method("display").with(eq(values));
-
+        
+        DatasetType datasetType = new DatasetType();
         Mock dataset = mock(EmfDataset.class);
         dataset.stubs().method("getKeyVals").will(returnValue(values));
+        dataset.stubs().method("getDatasetType").will(returnValue(datasetType));
 
         KeywordsTabPresenter presenter = new KeywordsTabPresenter(((KeywordsTabView) view.proxy()),
                 (EmfDataset) dataset.proxy());
