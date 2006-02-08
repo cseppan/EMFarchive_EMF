e47eda76b63f4935414170eb4e3aa8518d474106
rhavaldar <rhavaldar>
2006-02-08 16:05:43 +0000

    Modified LoggingServiceImpl to use dao instance (removed static calls) Modified Logging DAO to remove static methods.

--- src/services/gov/epa/emissions/framework/dao/LoggingDAO.java
+++ src/services/gov/epa/emissions/framework/dao/LoggingDAO.java
@@ -4,23 +4,39 @@ import gov.epa.emissions.framework.services.AccessLog;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class LoggingDAO {
     private static final String GET_ACCESS_LOG_QUERY = "from AccessLog as alog where alog.datasetId=:datasetid";
 
-    public static void insertAccessLog(AccessLog accesslog, Session session) {
-        Transaction tx = session.beginTransaction();
-        session.save(accesslog);
-        tx.commit();
+    public void insertAccessLog(AccessLog accesslog, Session session) {
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(accesslog);
+            tx.commit();
+        } catch (HibernateException e) {
+            tx.rollback();
+            throw e;
+        }
+
     }
 
-    public static List getAccessLogs(long datasetid, Session session) {
-        Transaction tx = session.beginTransaction();
-        List allLogs = session.createQuery(GET_ACCESS_LOG_QUERY).setLong("datasetid", datasetid).list();
-        tx.commit();
+    public List getAccessLogs(long datasetid, Session session) {
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            List allLogs = session.createQuery(GET_ACCESS_LOG_QUERY).setLong("datasetid", datasetid).list();
+            tx.commit();
 
-        return allLogs;
+            return allLogs;
+
+        } catch (HibernateException e) {
+            tx.rollback();
+            throw e;
+        }
+
     }
 }
--- src/services/gov/epa/emissions/framework/services/impl/LoggingServiceImpl.java
+++ src/services/gov/epa/emissions/framework/services/impl/LoggingServiceImpl.java
@@ -1,16 +1,22 @@
 package gov.epa.emissions.framework.services.impl;
 
+import gov.epa.emissions.framework.EmfException;
 import gov.epa.emissions.framework.dao.LoggingDAO;
 import gov.epa.emissions.framework.services.AccessLog;
 import gov.epa.emissions.framework.services.LoggingService;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
 
 public class LoggingServiceImpl implements LoggingService {
+    private static Log LOG = LogFactory.getLog(LoggingServiceImpl.class);
 
     private HibernateSessionFactory sessionFactory;
+
+    private LoggingDAO dao;
 
     public LoggingServiceImpl() {
         this(HibernateSessionFactory.get());
@@ -18,24 +24,34 @@ public class LoggingServiceImpl implements LoggingService {
 
     public LoggingServiceImpl(HibernateSessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
+        dao = new LoggingDAO();
     }
 
-    public void setAccessLog(AccessLog accesslog) {
-        Session session = sessionFactory.getSession();
+    public void setAccessLog(AccessLog accesslog) throws EmfException {
+        try {
+            Session session = sessionFactory.getSession();
+            dao.insertAccessLog(accesslog,session);
+            session.close();
 
-        LoggingDAO.insertAccessLog(accesslog, session);
-        session.flush();
-        session.close();
+        } catch (RuntimeException e) {
+            LOG.error("Could not insert access log - " + accesslog, e);
+            throw new EmfException("Could not insert access log");
+        }
+
     }
 
-    public AccessLog[] getAccessLogs(long datasetid) {
-        Session session = sessionFactory.getSession();
+    public AccessLog[] getAccessLogs(long datasetid) throws EmfException {
+        try {
+            Session session = sessionFactory.getSession();
+            List allLogs = dao.getAccessLogs(datasetid, session);
+            session.close();
 
-        List allLogs = LoggingDAO.getAccessLogs(datasetid, session);
-        session.flush();
-        session.close();
+            return (AccessLog[]) allLogs.toArray(new AccessLog[allLogs.size()]);
+        } catch (RuntimeException e) {
+            LOG.error("Could not get all access logs", e);
+            throw new EmfException("Could not get all access logs");
+        }
 
-        return (AccessLog[]) allLogs.toArray(new AccessLog[allLogs.size()]);
     }
 
 }
