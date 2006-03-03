ff1d5bf98683a655e9a72e8da7317e34c416abc2
rhavaldar <rhavaldar>
2006-03-03 15:27:58 +0000

    added version number in title bar

--- src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
+++ src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
@@ -45,7 +45,7 @@ public class LoginWindow extends EmfFrame implements LoginView {
     private ServiceLocator serviceLocator;
 
     public LoginWindow(ServiceLocator serviceLocator) {
-        super("Login", "Login to the Emissions Modeling Framework");
+        super("Login", "Login to the Emissions Modeling Framework v0.4");
         this.serviceLocator = serviceLocator;
 
         JPanel layoutPanel = createLayout();
