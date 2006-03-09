7c81d0bf2edeafd7722bf8c2cb53fa7624a541d1
parthee <parthee>
2006-03-09 14:39:22 +0000

    Added custom cell editors for data editor

--- src/client/gov/epa/emissions/framework/client/data/editor/DataEditorTable.java
+++ src/client/gov/epa/emissions/framework/client/data/editor/DataEditorTable.java
@@ -2,7 +2,6 @@ package gov.epa.emissions.framework.client.data.editor;
 
 import gov.epa.emissions.commons.gui.EditableTable;
 import gov.epa.emissions.commons.gui.EditableTableModel;
-import gov.epa.emissions.commons.io.ColumnMetaData;
 import gov.epa.emissions.commons.io.TableMetadata;
 import gov.epa.emissions.framework.client.data.DoubleRenderer;
 import gov.epa.emissions.framework.client.data.TableColumnHeaders;
@@ -11,46 +10,20 @@ import gov.epa.emissions.framework.ui.TableColumnWidth;
 
 public class DataEditorTable extends EditableTable {
 
-    private MessagePanel messagePanel;
-    
-    private TableMetadata tableMetadata;
-
     public DataEditorTable(EditableTableModel tableModel, TableMetadata tableMetadata, MessagePanel messagePanel) {
         super(tableModel);
-        this.tableMetadata = tableMetadata;
-        this.messagePanel = messagePanel;
-        new TableColumnHeaders(this,tableMetadata).renderHeader();
+        new TableColumnHeaders(this, tableMetadata).renderHeader();
         new TableColumnWidth(this, tableMetadata).columnWidths();
-        setDefaultRenderer(Double.class,new DoubleRenderer());
-        setDefaultRenderer(Float.class,new DoubleRenderer());
+        setDefaultRenderer(Double.class, new DoubleRenderer());
+        setDefaultRenderer(Float.class, new DoubleRenderer());
+        NumericTableCellEditor numericTableCellEditor = new NumericTableCellEditor(messagePanel);
+        setDefaultEditor(Double.class, numericTableCellEditor);
+        setDefaultEditor(Float.class, numericTableCellEditor);
+        setDefaultEditor(Integer.class, numericTableCellEditor);
+        setDefaultEditor(String.class, new StringTableCellEditor(tableMetadata, messagePanel));
     }
 
     public void setValueAt(Object value, int row, int column) {
-        messagePanel.clear();
-        if (getColumnClass(column) == String.class) {
-            value = validate((String) value, row, column);
-        }
         super.setValueAt(value, row, column);
     }
-
-    private Object validate(String value, int row, int column) {
-        ColumnMetaData metadata = tableMetadata.columnMetadata(getColumnName(column));
-        if (metadata == null) {
-            return value;
-        }
-        if (!sizeCheck(value, metadata)) {
-            messagePanel.setError("Enter a value no longer than " + metadata.getSize()+" characters");
-            return getValueAt(row, column); // return the current value;
-        }
-
-        return value;
-
-    }
-
-    private boolean sizeCheck(String value, ColumnMetaData metadata) {
-        int dbColumnSize = metadata.getSize();
-        // -1=> no size constraints
-        return (dbColumnSize == -1 || value == null || dbColumnSize >= value.length());
-    }
-
 }
