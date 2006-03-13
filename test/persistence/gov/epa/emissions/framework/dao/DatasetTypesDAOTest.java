01921d5ea99d51167a0a278884efdd91f2816818
rhavaldar <rhavaldar>
2006-03-13 22:30:53 +0000

    Verified that QA Step Templates can be updated in a Dataset Type

--- test/persistence/gov/epa/emissions/framework/dao/DatasetTypesDAOTest.java
+++ test/persistence/gov/epa/emissions/framework/dao/DatasetTypesDAOTest.java
@@ -130,7 +130,7 @@ public class DatasetTypesDAOTest extends ServiceTestCase {
 
             QAStepTemplate[] results = updated.getQaStepTemplates();
             assertEquals(1, results.length);
-            assertEquals(template.getName(), results[0].getName());
+            assertEquals("step1", results[0].getName());
         } finally {
             super.remove(type);
         }
@@ -156,6 +156,38 @@ public class DatasetTypesDAOTest extends ServiceTestCase {
             
             QAStepTemplate[] results = updated.getQaStepTemplates();
             assertEquals(0, results.length);
+        } finally {
+            super.remove(type);
+        }
+    }
+    
+    public void testShouldUpdateQAStepTemplate() throws Exception {
+        DatasetType type = new DatasetType();
+        type.setName("test-type");
+        type.setDescription("test-desc");
+        
+        QAStepTemplate template = new QAStepTemplate();
+        template.setName("step1");
+        type.addQaStepTemplate(template);
+        
+        super.add(type);
+        
+        try {
+            User owner = userDao.get("emf", session);
+            DatasetType modified = dao.obtainLocked(owner, type, session);
+            
+            QAStepTemplate[] templates = modified.getQaStepTemplates();
+            assertEquals(1, templates.length);
+            QAStepTemplate updateTemplate = templates[0];
+            updateTemplate.setName("updated-template");
+            updateTemplate.setProgram("program-1");
+            
+            DatasetType updated = dao.update(modified, session);
+            
+            QAStepTemplate[] results = updated.getQaStepTemplates();
+            assertEquals(1, results.length);
+            assertEquals("updated-template", results[0].getName());
+            assertEquals("program-1", results[0].getProgram());
         } finally {
             super.remove(type);
         }
