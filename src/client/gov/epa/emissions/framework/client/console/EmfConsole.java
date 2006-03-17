bd2536dd976b1fedb30fdfc7b23d9f933e318643
eyth <eyth>
2006-03-17 21:50:32 +0000

    changed version info

--- src/client/gov/epa/emissions/framework/client/console/EmfConsole.java
+++ src/client/gov/epa/emissions/framework/client/console/EmfConsole.java
@@ -35,7 +35,7 @@ public class EmfConsole extends EmfFrame implements EmfConsoleView {
     private StatusPresenter presenter;
 
     private static String aboutMessage = "<html><center>Emissions Modeling Framework (EMF)<br>"
-            + "Version 0.5.1 - 3/7/2006<br>" + "Developed by the Carolina Environmental Program<br>"
+            + "Version 0.6 - 3/21/2006<br>" + "Developed by the Carolina Environmental Program<br>"
             + "University of North Carolina at Chapel Hill</center></html>";
 
     private DesktopManager desktopManager;
