437cb0e8d91dfba554e1539118f0eeea6ba0134a
parthee <parthee>
2006-05-19 19:12:59 +0000

    name changes EditableCMSCCTab

--- src/client/gov/epa/emissions/framework/client/cost/controlmeasure/EditableCMSCCTab.java
+++ src/client/gov/epa/emissions/framework/client/cost/controlmeasure/EditableCMSCCTab.java
@@ -1,96 +1,47 @@
 package gov.epa.emissions.framework.client.cost.controlmeasure;
 
-import gov.epa.emissions.commons.gui.Button;
-import gov.epa.emissions.commons.gui.SortFilterSelectModel;
-import gov.epa.emissions.commons.gui.SortFilterSelectionPanel;
+import gov.epa.emissions.commons.gui.ManageChangeables;
 import gov.epa.emissions.framework.client.EmfSession;
-import gov.epa.emissions.framework.client.console.EmfConsole;
+import gov.epa.emissions.framework.services.EmfException;
 import gov.epa.emissions.framework.services.cost.ControlMeasure;
-import gov.epa.emissions.framework.ui.EmfTableModel;
+import gov.epa.emissions.framework.ui.EditableRow;
+import gov.epa.emissions.framework.ui.EditableTablePanel;
 import gov.epa.emissions.framework.ui.MessagePanel;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
+import java.util.List;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 public class EditableCMSCCTab extends JPanel implements EditableCMTabView {
 
-    private SortFilterSelectModel selectModel;
+    private EditableTablePanel panel;
 
-    private EmfConsole parentConsole;
+    private SCCTableData tableData;
 
-    private EmfTableModel model;
-
-    private JPanel mainPanel;
-
-    public EditableCMSCCTab(ControlMeasure measure, EmfSession session, MessagePanel messagePanel) {
-        this.mainPanel = new JPanel(new BorderLayout());
-        this.parentConsole = null;
-        doLayout(measure);
+    public EditableCMSCCTab(ControlMeasure measure, EmfSession session, ManageChangeables changeables,
+            MessagePanel messagePanel) {
+        doLayout(measure, changeables);
     }
 
-    private void doLayout(ControlMeasure measure) {
+    private void doLayout(ControlMeasure measure, ManageChangeables changeables) {
         String[] sccs = measure.getSccs();
-        updateMainPanel(sccs);
-
+        tableData = new SCCTableData(sccs);
+        panel = new EditableTablePanel("", tableData, changeables, null);
         setLayout(new BorderLayout());
-        add(mainPanel, BorderLayout.CENTER);
-        add(controlPanel(), BorderLayout.SOUTH);
+        add(panel, BorderLayout.CENTER);
     }
 
-    private void updateMainPanel(String[] sccs) {
-        mainPanel.removeAll();
-        initModel(sccs);
-        JScrollPane pane = sortFilterPane(parentConsole);// FIXME: pass the parentConsol
-        mainPanel.add(pane);
-    }
-
-    private void initModel(String[] sccs) {
-        model = new EmfTableModel(new SCCTableData(sccs));
-        selectModel = new SortFilterSelectModel(model);
-    }
-
-    private JScrollPane sortFilterPane(EmfConsole parentConsole) {
-        SortFilterSelectionPanel panel = new SortFilterSelectionPanel(parentConsole, selectModel);
-        panel.getTable().setName("controlMeasureSccTable");
-        panel.setPreferredSize(new Dimension(450, 120));
-
-        return new JScrollPane(panel);
-    }
-
-    private JPanel controlPanel() {
-        Button addButton = new Button("Add", addAction());
-        Button removeButton = new Button("Remove", removeAction());
-
-        JPanel panel = new JPanel();
-        panel.add(addButton);
-        panel.add(removeButton);
-        return panel;
-    }
-
-    private Action addAction() {
-        return new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                // bring the scc reference tables
-            }
-        };
-    }
-
-    private Action removeAction() {
-        return new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                // remove selected data
-            }
-        };
-    }
-
-    public void save(ControlMeasure measure) {
-        //
+    public void save(ControlMeasure measure) throws EmfException {
+        panel.commit();
+        List sccs = tableData.rows();
+        String[] newSccs = new String[sccs.size()];
+        for (int i = 0; i < sccs.size(); i++) {
+            EditableRow row = (EditableRow) sccs.get(i);
+            row.validate(i);
+            newSccs[i] = (String) row.source();
+        }
+        measure.setSccs(newSccs);
     }
 
 }
