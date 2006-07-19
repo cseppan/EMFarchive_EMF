678fb33bbef1df7c1548a19e176532385233f853
qunhe <qunhe>
2006-07-19 04:20:09 +0000

    Implementation of control strategy results summary in progress

--- src/contract/gov/epa/emissions/framework/services/cost/data/ControlStrategyResultsSummary.java
+++ src/contract/gov/epa/emissions/framework/services/cost/data/ControlStrategyResultsSummary.java
@@ -2,32 +2,20 @@ package gov.epa.emissions.framework.services.cost.data;
 
 import gov.epa.emissions.framework.services.cost.controlStrategy.StrategyResult;
 
-import java.io.Serializable;
+public class ControlStrategyResultsSummary {
 
-public class ControlStrategyResultsSummary implements Serializable {
-
-    private int id;
-    
     private StrategyResult[] strategyResults;
     
-    public ControlStrategyResultsSummary(){
-        //
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public StrategyResult[] getStrategyResults() {
-        return strategyResults;
-    }
-
-    public void setStrategyResults(StrategyResult[] strategyResults) {
+    public ControlStrategyResultsSummary(StrategyResult[] strategyResults){
         this.strategyResults = strategyResults;
     }
-    
+
+    public float getStrategyTotalCost() {
+        float totalCost = 0;
+        
+        for (int i = 0; i < strategyResults.length; i++)
+            totalCost += strategyResults[i].getTotalCost();
+        
+        return totalCost;
+    }
 }
