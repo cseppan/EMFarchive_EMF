6f7fec7ac6c7d632fecb59e56265e4fa4c61784c
parthee <parthee>
2006-09-01 21:12:10 +0000

    added error handling message for orl types other than orl nonpoin

--- src/services/gov/epa/emissions/framework/services/cost/RunControlStrategy.java
+++ src/services/gov/epa/emissions/framework/services/cost/RunControlStrategy.java
@@ -1,5 +1,6 @@
 package gov.epa.emissions.framework.services.cost;
 
+import gov.epa.emissions.commons.data.DatasetType;
 import gov.epa.emissions.commons.security.User;
 import gov.epa.emissions.framework.services.EmfException;
 import gov.epa.emissions.framework.services.GCEnforcerTask;
@@ -8,6 +9,7 @@ import gov.epa.emissions.framework.services.basic.LoggingServiceImpl;
 import gov.epa.emissions.framework.services.basic.StatusDAO;
 import gov.epa.emissions.framework.services.cost.analysis.Strategy;
 import gov.epa.emissions.framework.services.data.DataServiceImpl;
+import gov.epa.emissions.framework.services.data.EmfDataset;
 import gov.epa.emissions.framework.services.persistence.HibernateSessionFactory;
 
 import org.apache.commons.logging.Log;
@@ -35,17 +37,29 @@ public class RunControlStrategy {
     }
 
     public void run(User user, ControlStrategy controlStrategy, ControlStrategyService service) throws EmfException {
+        currentLimitations(controlStrategy);
         try {
-            Strategy strategy = factory.create(controlStrategy,user,sessionFactory);
+
+            Strategy strategy = factory.create(controlStrategy, user, sessionFactory);
             StrategyTask task = new StrategyTask(strategy, user, services, service, sessionFactory);
             threadPool.execute(new GCEnforcerTask("Run Strategy: " + controlStrategy.getName(), task));
         } catch (Exception e) {
             log.error("Error running control strategy: " + controlStrategy.getName(), e);
-
-            if (controlStrategy.getStrategyType().getName().equalsIgnoreCase("Least Cost"))
-                throw new EmfException("Least Cost Analysis is not supported.");
             throw new EmfException(e.getMessage());
         }
+    }
+
+    private void currentLimitations(ControlStrategy controlStrategy) throws EmfException {
+        if (controlStrategy.getStrategyType().getName().equalsIgnoreCase("Least Cost"))
+            throw new EmfException("Least Cost Analysis is not supported.");
+
+        EmfDataset[] inputDatasets = controlStrategy.getInputDatasets();
+        if (inputDatasets.length == 0)
+            return;
+        DatasetType datasetType = inputDatasets[0].getDatasetType();
+        if (!datasetType.getName().equalsIgnoreCase(("ORL Nonpoint Inventory (ARINV)")))
+            throw new EmfException("The dataset type '" + datasetType.getName() + "' is not support yet.");
+
     }
 
     public void stop() {
