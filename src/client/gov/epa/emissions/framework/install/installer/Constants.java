23d3a87785cbb51a8883e90f96172870c38d78c0
eyth <eyth>
2006-09-06 22:03:20 +0000

    changed date

--- src/client/gov/epa/emissions/framework/install/installer/Constants.java
+++ src/client/gov/epa/emissions/framework/install/installer/Constants.java
@@ -2,7 +2,7 @@ package gov.epa.emissions.framework.install.installer;
 
 
 public class Constants {
-	public static final String VERSION = "6/15/2006";
+	public static final String VERSION = "6/26/2006";
 	public static final String SEPARATOR = System.getProperty("line.separator");
     public static final String WORK_DIR = System.getProperty("user.dir");
 	
