98057c282e1863afca80e9d5d13d8b201e9b27a0
eyth <eyth>
2006-09-14 17:48:16 +0000

    no need to create new statuses each time

--- src/client/gov/epa/emissions/framework/client/casemanagement/RunStatuses.java
+++ src/client/gov/epa/emissions/framework/client/casemanagement/RunStatuses.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RunStatuses {
-
-    private List list;
-
-    public RunStatuses() {
-        this.list = new ArrayList();
+ 
+    private static String [] statusList;
+    
+    static
+    {
+        List list = new ArrayList();
 
         list.add("Not Started");
         list.add("Running");
         list.add("Failed");
         list.add("Complete");
+        
+        statusList = new String[4];
+        list.toArray(statusList);
     }
-
-    public String[] all() {
-        return (String[]) list.toArray(new String[0]);
+        
+    public static String[] all() {
+        return statusList;
     }
 }
