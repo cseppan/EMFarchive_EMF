6126449159856f24073461c399a20021b0939de4
eyth <eyth>
2006-12-20 21:31:43 +0000

    do not refresh dataset manager when clicking done

--- src/client/gov/epa/emissions/framework/client/exim/DatasetsBrowserAwareImportPresenter.java
+++ src/client/gov/epa/emissions/framework/client/exim/DatasetsBrowserAwareImportPresenter.java
@@ -3,7 +3,7 @@ package gov.epa.emissions.framework.client.exim;
 import gov.epa.emissions.commons.security.User;
 import gov.epa.emissions.framework.client.EmfSession;
 import gov.epa.emissions.framework.client.data.dataset.DatasetsBrowserView;
-import gov.epa.emissions.framework.services.EmfException;
+//import gov.epa.emissions.framework.services.EmfException;
 import gov.epa.emissions.framework.services.data.DataService;
 import gov.epa.emissions.framework.services.exim.ExImService;
 
@@ -12,24 +12,25 @@ import gov.epa.emissions.framework.services.exim.ExImService;
  */
 public class DatasetsBrowserAwareImportPresenter extends ImportPresenter {
 
-    private DataService dataServices;
+//    private DataService dataServices;
 
-    private DatasetsBrowserView browser;
+//    private DatasetsBrowserView browser;
 
     public DatasetsBrowserAwareImportPresenter(EmfSession session, User user, ExImService eximServices, DataService dataServices,
             DatasetsBrowserView browser) {
         super(session, user, eximServices);
-        this.dataServices = dataServices;
-        this.browser = browser;
+ //       this.dataServices = dataServices;
+ //       this.browser = browser;
     }
 
     public void doDone() {
-        try {
-            browser.refresh(dataServices.getDatasets());
-        } catch (EmfException e) {
-            browser.showError("Could not refresh Datasets");
-            return;
-        }
+        // Do not refresh due to time required
+//        try {
+//            browser.refresh(dataServices.getDatasets());
+//        } catch (EmfException e) {
+//            browser.showError("Could not refresh Datasets");
+//            return;
+//        }
 
         super.doDone();
     }
