776b77d815c6db4a05c26f89f6bf44cb11ac9ef3
eyth <eyth>
2007-01-26 01:02:41 +0000

    added method to return final version objects

--- src/client/gov/epa/emissions/framework/client/meta/versions/VersionsSet.java
+++ src/client/gov/epa/emissions/framework/client/meta/versions/VersionsSet.java
@@ -32,6 +32,16 @@ public class VersionsSet {
         return (Integer[]) list.toArray(new Integer[0]);
     }
 
+    public Version[] finalVersionObjects() {
+        List list = new ArrayList();
+        for (int i = 0; i < versions.length; i++) {
+            if (versions[i].isFinalVersion())
+                list.add(versions[i]);
+        }
+
+        return (Version[]) list.toArray(new Version[0]);
+    }
+    
     public String[] namesOfFinalVersions() {
         List list = new ArrayList();
         for (int i = 0; i < versions.length; i++) {
