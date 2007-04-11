6d1a77f4a01dfde623c0d5915737287a91b0c580
qunhe <qunhe>
2007-04-11 14:43:49 +0000

    Switched "source" and "size" column positions on dataset properties editor's table tab.

--- src/client/gov/epa/emissions/framework/client/meta/info/InternalSourcesTableData.java
+++ src/client/gov/epa/emissions/framework/client/meta/info/InternalSourcesTableData.java
@@ -17,7 +17,7 @@ public class InternalSourcesTableData extends AbstractTableData {
     }
 
     public String[] columns() {
-        return new String[] { "Table", "Type", "Source", "Size" };
+        return new String[] { "Table", "Type", "Size", "Source" };
     }
 
     public List rows() {
@@ -37,8 +37,8 @@ public class InternalSourcesTableData extends AbstractTableData {
 
         for (int i = 0; i < sources.length; i++) {
             InternalSource element = sources[i];
-            Object[] values = { element.getTable(), element.getType(), element.getSource(),
-                    new Long(element.getSourceSize()) };
+            Object[] values = { element.getTable(), element.getType(),
+                    new Long(element.getSourceSize()), element.getSource() };
 
             Row row = new ViewableRow(element, values);
             rows.add(row);
