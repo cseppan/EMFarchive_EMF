22632ac17273921db0f4d070d0ca462d52bf6039
netswirl <netswirl>
2007-07-10 19:22:40 +0000

    New class for command line framework client

---
+++ src/client/gov/epa/emissions/framework/client/EMFCmdClient.java
@@ -0,0 +1,62 @@
+package gov.epa.emissions.framework.client;
+
+import gov.epa.emissions.framework.client.transport.RemoteServiceLocator;
+import gov.epa.emissions.framework.client.transport.ServiceLocator;
+import gov.epa.emissions.framework.services.basic.UserService;
+
+
+public class EMFCmdClient {
+    private static final String DEFAULT_URL = "http://localhost:8080/emf/services";// default
+
+
+    private static UserService userAdmin;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 1 && args[0].equalsIgnoreCase("Help")) {
+            displayHelp();
+            return;
+        }
+
+        run(args);
+
+    }
+
+    private static void displayHelp() {
+        System.out
+                .println("Usage\njava "
+                        + EMFClient.class.getName()
+                        + " [url]"
+                        + "\n\turl - location of EMF Services. Defaults to "
+                        + DEFAULT_URL
+                        + "\n\tspecify '-DUSER_PREFERENCES=<full path to EMFPrefs.txt>' to override location of User Preferences");
+    }
+
+    
+    /**
+     *
+     */
+    private static void run(String[] args) throws Exception {
+        try
+        {
+          String url = DEFAULT_URL;
+          if (args.length == 1)
+              url = args[0];
+
+          System.out.println("Starting EMF Command Client");
+          ServiceLocator serviceLocator = new RemoteServiceLocator(url);
+
+          userAdmin=serviceLocator.userService();
+         
+          System.out.println("EMF VERSION INFO: " + userAdmin.getEmfVersion());
+          System.out.println("Exiting EMF Command Client");
+          
+        }
+        catch (Exception exc)
+        {
+            System.out.println("Exception starting client: "+exc.getMessage());
+            exc.printStackTrace();
+            throw exc;
+        }
+      }
+
+}
