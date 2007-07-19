6791f148485d412332d76bf168c3ef07c224487e
qunhe <qunhe>
2007-07-19 15:20:43 +0000

    Added a new data format as yyyyMMDDHHmmss as needed.

--- src/contract/gov/epa/emissions/framework/services/data/EmfDateFormat.java
+++ src/contract/gov/epa/emissions/framework/services/data/EmfDateFormat.java
@@ -63,7 +63,7 @@ public class EmfDateFormat {
         return date == null ? null : dateFormatter.parse(date);
     }
 
-    public static String parse_YYYYMMDDHHMMss(Date date) {
+    public static String parse_YYYYMMDDHHMMSS(Date date) {
         dateFormatter.applyPattern("yyyyMMddHHmmss");
         return date == null ? "" : dateFormatter.format(date);
     }
