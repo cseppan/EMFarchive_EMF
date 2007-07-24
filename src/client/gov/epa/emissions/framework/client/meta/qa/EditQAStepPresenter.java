02f70367caaa92d2e35d10617e97dc88af520a88
qunhe <qunhe>
2007-07-24 20:45:22 +0000

    Modified qa step editor presenter to save the export dir once the qa step result is exported.

--- src/client/gov/epa/emissions/framework/client/meta/qa/EditQAStepPresenter.java
+++ src/client/gov/epa/emissions/framework/client/meta/qa/EditQAStepPresenter.java
@@ -20,7 +20,7 @@ public class EditQAStepPresenter {
     private EmfDataset dataset;
 
     private EditableQATabView tabView;
-    
+
     private QAStep qastep;
 
     private EmfSession session;
@@ -40,7 +40,7 @@ public class EditQAStepPresenter {
         QAProgram[] programs = qaService.getQAPrograms();
         QAStepResult result = qaService.getQAStepResult(step);
         view.display(step, result, programs, dataset, versionName, session);
-        
+
         // Reversed the following line from behind the one after it to make sure that most recent folder
         // is displayed properly.
         this.qastep = step;
@@ -69,13 +69,13 @@ public class EditQAStepPresenter {
     }
 
     public void export(QAStep qaStep, QAStepResult stepResult, String dirName) throws EmfException {
-        File dir = new File(dirName);
-        if (dir.isDirectory())
-            lastFolder = dirName;
+        lastFolder = dirName;
 
         if (stepResult == null || stepResult.getTable() == null)
             throw new EmfException("You must have run the QA step successfully before exporting ");
 
+        qaStep.setOutputFolder(dirName);
+        session.qaService().updateWitoutCheckingConstraints(new QAStep[] { qaStep });
         session.qaService().exportQAStep(qaStep, session.user(), dirName);
     }
 
@@ -88,15 +88,15 @@ public class EditQAStepPresenter {
         // because qastep is null.
         String folder;
         if (qastep != null) {
-        //String folder = session.preferences().outputFolder();
+            // String folder = session.preferences().outputFolder();
             folder = qastep.getOutputFolder();
         } else {
             folder = session.preferences().outputFolder();
         }
-        //if (!new File(folder).isDirectory())
-        
-        //Added extra logical construct to handle a null folder
-        
+        // if (!new File(folder).isDirectory())
+
+        // Added extra logical construct to handle a null folder
+
         if (folder != null && !new File(folder).isDirectory())
             folder = "";// default, if unspecified
 
@@ -106,31 +106,32 @@ public class EditQAStepPresenter {
     public String userName() {
         return session.user().getUsername();
     }
-    
+
     public void viewResults(QAStep qaStep, String exportDir) throws EmfException {
         QAStepResult qaResult = getStepResult(qaStep);
-        
+
         if (qaResult == null)
             throw new EmfException("Please run the QA Step before viewing the result.");
-        
+
         File exported;
         try {
             RemoteCopy remoteCopy = new RemoteCopy(new DefaultUserPreferences(), session.user());
             String copied = remoteCopy.copyToLocal(exportedQAStepFilePath(exportDir, qaResult), "");
             exported = new File(copied);
-            
+
             if (!exported.exists())
                 throw new EmfException("Copy remote files failed.");
         } catch (Exception e) {
             throw new EmfException(e.getMessage());
         }
-        
+
         view.displayResultsTable(qaStep.getName(), exported.getAbsolutePath());
     }
-    
+
     private String exportedQAStepFilePath(String exportDir, QAStepResult qaStepResult) {
         String separator = (exportDir.charAt(0) == '/') ? "/" : "\\";
-        return exportDir + separator + qaStepResult.getTable() + ".csv"; // this is how exported file name was created
+        return exportDir + separator + qaStepResult.getTable() + ".csv"; // this is how exported file name was
+                                                                            // created
     }
 
     public QAStepResult getStepResult(QAStep step) throws EmfException {
