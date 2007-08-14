b496a61aa3ca693add7345c9a8fa53f83516b925
netswirl <netswirl>
2007-08-14 19:30:48 +0000

    Added DebugLevels logging (TEMPORARILY)

--- src/services/gov/epa/emissions/framework/services/EmfServiceImpl.java
+++ src/services/gov/epa/emissions/framework/services/EmfServiceImpl.java
@@ -4,6 +4,7 @@ import gov.epa.emissions.commons.PerformanceMetrics;
 import gov.epa.emissions.commons.db.DbServer;
 import gov.epa.emissions.framework.services.data.EmfDateFormat;
 import gov.epa.emissions.framework.services.persistence.DataSourceFactory;
+import gov.epa.emissions.framework.tasks.DebugLevels;
 
 import java.util.Date;
 
@@ -22,12 +23,14 @@ public abstract class EmfServiceImpl {
     private String name;
 
     public EmfServiceImpl(String name) throws Exception {
+
         this.name = name;
         datasource = new DataSourceFactory().get();
-        
+
         dbServer = new EmfDbServer();
         LOG.debug("Starting  " + name + "(" + this.hashCode() + ")");
-        System.out.println("Starting  " + name + "(" + this.hashCode() + "): "+EmfDateFormat.format_YYYY_MM_DD_HH_MM(new Date()));
+        System.out.println("Starting  " + name + "(" + this.hashCode() + "): "
+                + EmfDateFormat.format_YYYY_MM_DD_HH_MM(new Date()));
     }
 
     public EmfServiceImpl(DataSource datasource, DbServer dbServer) {
@@ -38,7 +41,9 @@ public abstract class EmfServiceImpl {
     protected void finalize() throws Throwable {
         dbServer.disconnect();
         new PerformanceMetrics().gc("Shutting down " + name + "(" + this.hashCode() + ")");
-        System.out.println("Shutting down  " + name + "(" + this.hashCode() + "): "+EmfDateFormat.format_YYYY_MM_DD_HH_MM(new Date()));
+        if (DebugLevels.DEBUG_0)
+            System.out.println("Shutting down  " + name + "(" + this.hashCode() + "): "
+                    + EmfDateFormat.format_YYYY_MM_DD_HH_MM(new Date()));
         super.finalize();
     }
 
