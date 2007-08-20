4c652fa98858697da8f415ff5e3390ce6f227b1e
netswirl <netswirl>
2007-08-20 17:46:46 +0000

    no comment

--- src/services/gov/epa/emissions/framework/tasks/DebugLevels.java
+++ src/services/gov/epa/emissions/framework/tasks/DebugLevels.java
@@ -3,11 +3,11 @@ package gov.epa.emissions.framework.tasks;
 public class DebugLevels {
 
     public static final boolean DEBUG_0 = true;
-	public static final boolean DEBUG_1 = false;
-	public static final boolean DEBUG_2 = false;
-	public static final boolean DEBUG_3 = false;
-	public static final boolean DEBUG_4 = false;
-    public static final boolean DEBUG_5 = false;
+	public static final boolean DEBUG_1 = true;
+	public static final boolean DEBUG_2 = true;
+	public static final boolean DEBUG_3 = true;
+	public static final boolean DEBUG_4 = true;
+    public static final boolean DEBUG_5 = true;
     public static final boolean DEBUG_6 = true;
     
 
