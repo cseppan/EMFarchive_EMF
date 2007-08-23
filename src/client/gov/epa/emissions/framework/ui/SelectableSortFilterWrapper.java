26f276a165725b28ef2a523c99e975fe65d86a0c
eyth <eyth>
2007-08-23 21:47:42 +0000

    added comments

--- src/client/gov/epa/emissions/framework/ui/SelectableSortFilterWrapper.java
+++ src/client/gov/epa/emissions/framework/ui/SelectableSortFilterWrapper.java
@@ -5,6 +5,7 @@ import gov.epa.emissions.commons.gui.SortFilterSelectModel;
 import gov.epa.emissions.commons.gui.SortFilterSelectionPanel;
 import gov.epa.emissions.framework.client.console.EmfConsole;
 import gov.epa.mims.analysisengine.table.sort.SortCriteria;
+//TBD: import gov.epa.mims.analysisengine.table.filter.FilterCriteria;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -20,6 +21,8 @@ public class SelectableSortFilterWrapper extends JPanel implements SelectModel {
     private SortFilterSelectModel selectModel;
 
     private SortCriteria sortCriteria;
+    
+    //TBD: private FilterCriteria filterCriteria;
 
     private SortFilterSelectionPanel sortFilterSelectionpanel;
 
@@ -48,8 +51,12 @@ public class SelectableSortFilterWrapper extends JPanel implements SelectModel {
     }
 
     public void refresh(TableData tableData) {
+        // TBD: refine this to get the sort criteria and filter criteria from the SFSP
+        //      this may require adding some new methods to get this info from that class
         this.removeAll();
         this.add(setLayout(parentConsole, tableData));
+        // TBD: restore the sort and filter criteria (maybe after repaint??)
+        // TBD: if possible, retain the column widths and order in addition to sort and filter info
         repaint();
     }
 
