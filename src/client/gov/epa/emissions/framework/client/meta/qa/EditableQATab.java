1eec04d6dc6b5ef28a1bfe1fe40e2110f35c6e37
eyth <eyth>
2007-09-20 03:39:11 +0000

    updated sort order to include name

--- src/client/gov/epa/emissions/framework/client/meta/qa/EditableQATab.java
+++ src/client/gov/epa/emissions/framework/client/meta/qa/EditableQATab.java
@@ -82,8 +82,8 @@ public class EditableQATab extends JPanel implements EditableQATabView {
     }
 
     private SortCriteria sortCriteria() {
-        String[] columnNames = { "Version", "Order" };
-        return new SortCriteria(columnNames, new boolean[] { false, true }, new boolean[] { true, true });
+        String[] columnNames = { "Version", "Order", "Name" };
+        return new SortCriteria(columnNames, new boolean[] { false, true, true }, new boolean[] { true, true, true });
         //return new SortCriteria(columnNames, new boolean[] { false, false }, new boolean[] { true, true });
     }
 
