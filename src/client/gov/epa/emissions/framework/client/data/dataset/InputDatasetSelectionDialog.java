41d11710e2dd9c0b943bf5c120f4edca68dde1c8
dyang02 <dyang02>
2008-01-24 21:07:05 +0000

    add name contains filter

--- src/client/gov/epa/emissions/framework/client/data/dataset/InputDatasetSelectionDialog.java
+++ src/client/gov/epa/emissions/framework/client/data/dataset/InputDatasetSelectionDialog.java
@@ -3,8 +3,10 @@ package gov.epa.emissions.framework.client.data.dataset;
 import gov.epa.emissions.commons.data.DatasetType;
 import gov.epa.emissions.commons.gui.ComboBox;
 import gov.epa.emissions.commons.gui.ManageChangeables;
+import gov.epa.emissions.commons.gui.TextField;
 import gov.epa.emissions.commons.gui.buttons.CancelButton;
 import gov.epa.emissions.commons.gui.buttons.OKButton;
+import gov.epa.emissions.framework.client.SpringLayoutGenerator;
 import gov.epa.emissions.framework.client.console.EmfConsole;
 import gov.epa.emissions.framework.client.meta.EmfImageTool;
 import gov.epa.emissions.framework.services.EmfException;
@@ -21,11 +23,13 @@ import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.SpringLayout;
 
 public class InputDatasetSelectionDialog extends JDialog implements InputDatasetSelectionView {
 
@@ -33,6 +37,8 @@ public class InputDatasetSelectionDialog extends JDialog implements InputDataset
 
     private InputDatasetSelectionPresenter presenter;
 
+    private TextField name; 
+    
     private ComboBox datasetTypeCombo;
 
     private JList datasetList;
@@ -55,7 +61,7 @@ public class InputDatasetSelectionDialog extends JDialog implements InputDataset
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout(5, 5));
         JPanel panel = new JPanel(new BorderLayout(10, 10));
-        panel.add(buildDatasetTypeCombo(datasetTypes, defaultType), BorderLayout.NORTH);
+        panel.add(buildTopPanel(datasetTypes, defaultType), BorderLayout.NORTH);
         panel.add(buildDatasetsPanel(), BorderLayout.CENTER);
         panel.add(buttonPanel(), BorderLayout.SOUTH);
         contentPane.add(panel);
@@ -80,6 +86,14 @@ public class InputDatasetSelectionDialog extends JDialog implements InputDataset
     public EmfDataset[] getDatasets() {
         return datasets;
     }
+    
+    private JPanel buildTopPanel(DatasetType[] datasetTypes, DatasetType defaultType){
+        JPanel panel = new JPanel ();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(buildNameContains());
+        panel.add(buildDatasetTypeCombo(datasetTypes, defaultType));
+        return panel; 
+    }
 
     private JPanel buildDatasetTypeCombo(DatasetType[] datasetTypes, DatasetType defaultType) {
         JPanel panel = new JPanel(new BorderLayout());
@@ -88,7 +102,11 @@ public class InputDatasetSelectionDialog extends JDialog implements InputDataset
         datasetTypeCombo.addActionListener(new AbstractAction() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    presenter.refreshDatasets((DatasetType) datasetTypeCombo.getSelectedItem(), "");
+                    if (datasetTypeCombo.getSelectedItem() == null ){
+                        refreshDatasets(new EmfDataset[] {});
+                        return; 
+                    }  
+                    presenter.refreshDatasets((DatasetType) datasetTypeCombo.getSelectedItem(), name.getText());
                 } catch (EmfException e1) {
                     // NOTE Auto-generated catch block
                 }
@@ -96,10 +114,23 @@ public class InputDatasetSelectionDialog extends JDialog implements InputDataset
         });
 
         panel.add(datasetTypeCombo, BorderLayout.CENTER);
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 5, 20));
+        panel.setBorder(BorderFactory.createEmptyBorder(1, 20, 5, 20));
         return panel;
     }
 
+    private JPanel buildNameContains(){
+        JPanel panel = new JPanel(new SpringLayout()); 
+        SpringLayoutGenerator layoutGenerator = new SpringLayoutGenerator();
+        name= new  TextField ("Name contains", "", 25);
+        name.setEditable(true);
+        
+        layoutGenerator.addLabelWidgetPair("Name contains:  ", name, panel);
+        layoutGenerator.makeCompactGrid(panel, 1, 2, // rows, cols
+                25, 10, // initialX, initialY
+                5, 5);// xPad, yPad
+        return panel; 
+    }
+    
     private JPanel buildDatasetsPanel() {
         datasetList = new JList();
         datasetList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
