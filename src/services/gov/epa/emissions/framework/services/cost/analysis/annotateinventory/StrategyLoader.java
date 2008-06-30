66ad276c5d9ba3bd4cb8af099cc46120c1b3b01b
ddelvecchio <ddelvecchio>
2008-06-30 18:53:16 +0000

    got rid of checking if target pollutant was in the inventory, this type of strategy doesn't worry about a target pollutant.

--- src/services/gov/epa/emissions/framework/services/cost/analysis/annotateinventory/StrategyLoader.java
+++ src/services/gov/epa/emissions/framework/services/cost/analysis/annotateinventory/StrategyLoader.java
@@ -32,10 +32,7 @@ public class StrategyLoader extends AbstractStrategyLoader {
         EmfDataset inputDataset = controlStrategyInputDataset.getInputDataset();
         //make sure inventory has indexes created...
         makeSureInventoryDatasetHasIndexes(controlStrategyInputDataset);
-        //make sure inventory has the target pollutant, if not don't run
-        if (!inventoryHasTargetPollutant(controlStrategyInputDataset)) {
-            throw new EmfException("Error processing input dataset: " + controlStrategyInputDataset.getInputDataset().getName() + ". Target pollutant, " + controlStrategy.getTargetPollutant().getName() + ", is not in the inventory.");
-        }
+
         //reset counters
         recordCount = 0;
         totalCost = 0.0;
