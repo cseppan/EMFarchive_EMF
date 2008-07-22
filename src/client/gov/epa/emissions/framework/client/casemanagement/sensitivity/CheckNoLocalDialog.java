b5253e4249c2e112dbe03fe875bd2554916019e6
dyang02 <dyang02>
2008-07-22 13:39:47 +0000

    add OK button on bottom

--- src/client/gov/epa/emissions/framework/client/casemanagement/sensitivity/CheckNoLocalDialog.java
+++ src/client/gov/epa/emissions/framework/client/casemanagement/sensitivity/CheckNoLocalDialog.java
@@ -1,14 +1,22 @@
 package gov.epa.emissions.framework.client.casemanagement.sensitivity;
 
+import gov.epa.emissions.commons.gui.Button;
 import gov.epa.emissions.commons.gui.ScrollableComponent;
 import gov.epa.emissions.commons.gui.TextArea;
+import gov.epa.emissions.commons.gui.buttons.OKButton;
 import gov.epa.emissions.framework.client.console.EmfConsole;
 import gov.epa.emissions.framework.services.EmfException;
 import gov.epa.emissions.framework.services.casemanagement.Case;
 import gov.epa.emissions.framework.ui.Dialog;
 import gov.epa.mims.analysisengine.gui.ScreenUtils;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
 
 public class CheckNoLocalDialog  extends Dialog{
     
@@ -31,13 +39,31 @@ public class CheckNoLocalDialog  extends Dialog{
     
     public void display(String msg) {
         //String validationMsg = validateValues();
-        int width = 50;
-        int height = (msg.length() / 50)+3;
-        getContentPane().add(createMsgScrollPane(msg, width, height));
+        getContentPane().add(createPanel(msg));
         setLocation(ScreenUtils.getPointToCenter(parentConsole));
         pack();
         setModal(false);
         setVisible(true);
+    }
+    
+    private JPanel createPanel(String msg){
+        JPanel panel = new JPanel(new BorderLayout());
+        Button ok = new OKButton(new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                close();
+            }
+        });
+        
+        int width = 50;
+        int height = (msg.length() / 50)+3;
+        //getContentPane().add(createMsgScrollPane(msg, width, 20));
+        panel.add(createMsgScrollPane(msg, width, height));
+        
+        JPanel okPanel = new JPanel();
+        okPanel.add(ok);
+        panel.add(okPanel, BorderLayout.SOUTH);
+        panel.setBorder(BorderFactory.createEmptyBorder(5, 0, 10, 0));
+        return panel;
     }
     
     private ScrollableComponent createMsgScrollPane(String msg, int width, int height) {
