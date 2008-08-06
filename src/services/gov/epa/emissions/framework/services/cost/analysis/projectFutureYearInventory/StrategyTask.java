b4aae30f7b67d94d426dd544442d58ebe7d172a1
ddelvecchio <ddelvecchio>
2008-08-06 08:01:17 +0000

    added some pre validation code and also enhanced the way we null out missing values in the different control program datasets.

--- src/services/gov/epa/emissions/framework/services/cost/analysis/projectFutureYearInventory/StrategyTask.java
+++ src/services/gov/epa/emissions/framework/services/cost/analysis/projectFutureYearInventory/StrategyTask.java
@@ -1,9 +1,11 @@
 package gov.epa.emissions.framework.services.cost.analysis.projectFutureYearInventory;
 
+import gov.epa.emissions.commons.data.DatasetType;
 import gov.epa.emissions.commons.security.User;
 import gov.epa.emissions.framework.services.DbServerFactory;
 import gov.epa.emissions.framework.services.EmfException;
 import gov.epa.emissions.framework.services.cost.ControlProgram;
+import gov.epa.emissions.framework.services.cost.ControlProgramType;
 import gov.epa.emissions.framework.services.cost.ControlStrategy;
 import gov.epa.emissions.framework.services.cost.ControlStrategyInputDataset;
 import gov.epa.emissions.framework.services.cost.analysis.common.AbstractStrategyTask;
@@ -98,29 +100,79 @@ public class StrategyTask extends AbstractStrategyTask {
         //
     }
 
-    public void beforeRun() {
-        for (ControlProgram controlProgram : controlStrategy.getControlPrograms()) {
-            try {
-                convertMissingColumnValues(controlProgram.getDataset());
-            } catch (EmfException e) {
-                //
+    public void beforeRun() throws EmfException {
+        try {
+            //next lets replace missing data...
+            for (ControlProgram controlProgram : controlStrategy.getControlPrograms()) {
+                    convertMissingColumnValues(controlProgram);
             }
+            //next lets validate the control program dataset tables, make sure format is correct, no data is missing, etc...
+            validateControlPrograms();
+        } catch (EmfException e) {
+            throw e;
         }
     }
 
-    private void convertMissingColumnValues(EmfDataset dataset) throws EmfException {
-        String query = "";
-        query = "update " + qualifiedEmissionTableName(dataset)
-                + " set pointid = case when coalesce(pointid, '-9') = '-9' or trim(pointid) = '' then null::character varying(15) else pointid end, "
-                + " stackid = case when coalesce(stackid, '-9') = '-9' or trim(stackid) = '' then null::character varying(15) else stackid end, "
-                + " segment = case when coalesce(segment, '-9') = '-9' or trim(segment) = '' then null::character varying(15) else segment end "
-        		+ " where pointid = '-9' or stackid = '-9' or segment = '-9' or trim(pointid) = '' or trim(stackid) = '' or trim(segment) = '';"
-        		+ " analyze "  + qualifiedEmissionTableName(dataset) + ";";
-        System.out.println(System.currentTimeMillis() + " " + query);
+    private void validateControlPrograms() throws EmfException {
         try {
-            datasource.query().executeQuery(query);
+            datasource.query().execute("select public.validate_project_future_year_inventory_control_programs(" + controlStrategy.getId() + "::integer);");
         } catch (SQLException e) {
-            throw new EmfException("Could not execute query -" + query + "\n" + e.getMessage());
+            throw new EmfException(e.getMessage());
+        } finally {
+            //
+        }
+    }
+
+    private void convertMissingColumnValues(ControlProgram controlProgram) {
+        String query = "";
+        EmfDataset dataset = controlProgram.getDataset();
+        if (controlProgram.getControlProgramType().getName().equals(ControlProgramType.plantClosure)) {
+            query = "update " + qualifiedEmissionTableName(dataset)
+            + " set pointid = case when coalesce(pointid, '-9') = '-9' or trim(pointid) = '' then null::character varying(15) else pointid end, "
+            + " stackid = case when coalesce(stackid, '-9') = '-9' or trim(stackid) = '' then null::character varying(15) else stackid end, "
+            + " segment = case when coalesce(segment, '-9') = '-9' or trim(segment) = '' then null::character varying(15) else segment end "
+            + " where pointid = '-9' or stackid = '-9' or segment = '-9' or trim(pointid) = '' or trim(stackid) = '' or trim(segment) = '';"
+            + " analyze "  + qualifiedEmissionTableName(dataset) + ";";
+            query = "update " + qualifiedEmissionTableName(dataset)
+            + "         set plantid = case when plantid is null or trim(plantid) = '0' or trim(plantid) = '-9' or trim(plantid) = '' then null::character varying(15) else plantid end,"
+            + "         pointid = case when pointid is null or trim(pointid) = '0' or trim(pointid) = '-9' or trim(pointid) = '' then null::character varying(15) else pointid end,"
+            + "         stackid = case when stackid is null or trim(stackid) = '0' or trim(stackid) = '-9' or trim(stackid) = '' then null::character varying(15) else stackid end,"
+            + "         segment = case when segment is null or trim(segment) = '0' or trim(segment) = '-9' or trim(segment) = '' then null::character varying(15) else segment end,"
+            + "         fips = case when fips is null or trim(fips) = '0' or trim(fips) = '-9' or trim(fips) = '' then null::character varying(6) else fips end "
+            + "where trim(plantid) in ('0','-9','')"
+            + "         or trim(pointid) in ('0','-9','')"
+            + "         or trim(stackid) in ('0','-9','')"
+            + "         or trim(segment) in ('0','-9','')"
+            + "         or trim(fips) in ('0','-9','');"
+            + " analyze "  + qualifiedEmissionTableName(dataset) + ";";
+        } else if (controlProgram.getControlProgramType().getName().equals(ControlProgramType.projection)) {
+            if (dataset.getDatasetType().getName().equals(DatasetType.projectionPacket)) {
+                query = "update " + qualifiedEmissionTableName(dataset)
+                + "         set plantid = case when plantid is null or trim(plantid) = '0' or trim(plantid) = '-9' or trim(plantid) = '' then null::character varying(15) else plantid end,"
+                + "         pointid = case when pointid is null or trim(pointid) = '0' or trim(pointid) = '-9' or trim(pointid) = '' then null::character varying(15) else pointid end,"
+                + "         stackid = case when stackid is null or trim(stackid) = '0' or trim(stackid) = '-9' or trim(stackid) = '' then null::character varying(15) else stackid end,"
+                + "         segment = case when segment is null or trim(segment) = '0' or trim(segment) = '-9' or trim(segment) = '' then null::character varying(15) else segment end,"
+                + "         fips = case when fips is null or trim(fips) = '0' or trim(fips) = '-9' or trim(fips) = '' then null::character varying(6) else fips end,"
+                + "         scc = case when scc is null or trim(scc) = '0' or trim(scc) = '-9' or trim(scc) = '' then null::character varying(10) else scc end,"
+                + "         mact = case when mact is null or trim(mact) = '0' or trim(mact) = '-9' or trim(mact) = '' then null::character varying(6) else mact end,"
+                + "         sic = case when sic is null or trim(sic) = '0' or trim(sic) = '-9' or trim(sic) = '' then null::character varying(4) else sic end "
+                + "where trim(plantid) in ('0','-9','')"
+                + "         or trim(pointid) in ('0','-9','')"
+                + "         or trim(stackid) in ('0','-9','')"
+                + "         or trim(segment) in ('0','-9','')"
+                + "         or trim(fips) in ('0','-9','')"
+                + "         or trim(scc) in ('0','-9','')"
+                + "         or trim(mact) in ('0','-9','')"
+                + "         or trim(sic) in ('0','-9','');"
+                + " analyze "  + qualifiedEmissionTableName(dataset) + ";";
+            }
+        }
+        
+//        System.out.println(System.currentTimeMillis() + " " + query);
+        try {
+            datasource.query().execute(query);
+        } catch (SQLException e) {
+            //throw new EmfException("Could not execute query -" + query + "\n" + e.getMessage());
         } finally {
             //
         }
