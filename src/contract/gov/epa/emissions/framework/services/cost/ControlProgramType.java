eccd62587880631f385b5a49252c45bd08820579
ddelvecchio <ddelvecchio>
2008-08-06 07:58:12 +0000

    added new static variable to hold the projection control program type name

--- src/contract/gov/epa/emissions/framework/services/cost/ControlProgramType.java
+++ src/contract/gov/epa/emissions/framework/services/cost/ControlProgramType.java
@@ -10,7 +10,8 @@ public class ControlProgramType implements Serializable, Comparable {
 
     private String description;
 
-//    public static final String leastCost = "Least Cost";
+    public static final String plantClosure = "Plant Closure";
+    public static final String projection = "Projection";
 
     public ControlProgramType() {
         //
