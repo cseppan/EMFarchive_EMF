861c4e34cf09d65a34b8cd4ee0cc93e591aac3eb
qunhe <qunhe>
2008-08-12 16:22:16 +0000

    Increased the timeout threshold for remote file browser.

--- src/client/gov/epa/emissions/framework/client/transport/DataCommonsServiceTransport.java
+++ src/client/gov/epa/emissions/framework/client/transport/DataCommonsServiceTransport.java
@@ -437,7 +437,7 @@ public class DataCommonsServiceTransport implements DataCommonsService {
         call.setOperation("getSubdirs");
         call.addParam("dir", mappings.emfFileInfo());
         call.setReturnType(mappings.emfFileInfos());
-        call.setTimeOut(10000); //set time out in milliseconds to terminate if service doesn't response
+        call.setTimeOut(30000); //set time out in milliseconds to terminate if service doesn't response
         
         return (EmfFileInfo[])call.requestResponse(new Object[] { dir });
     }
@@ -449,7 +449,7 @@ public class DataCommonsServiceTransport implements DataCommonsService {
         call.addParam("dir", mappings.emfFileInfo());
         call.addStringParam("filter");
         call.setReturnType(mappings.emfFileInfos());
-        call.setTimeOut(10000); //set time out in milliseconds to terminate if service doesn't response
+        call.setTimeOut(30000); //set time out in milliseconds to terminate if service doesn't response
         
         return (EmfFileInfo[])call.requestResponse(new Object[] { dir, filter });
     }
