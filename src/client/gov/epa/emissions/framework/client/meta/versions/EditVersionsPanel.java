56b1c52892754068fcc0f1564b9244e4ef85d8a5
qunhe <qunhe>
2009-01-22 19:24:57 +0000

    Corrected an error in showing version object id instead of version number when ask user to make it default version for dataset.

--- src/client/gov/epa/emissions/framework/client/meta/versions/EditVersionsPanel.java
+++ src/client/gov/epa/emissions/framework/client/meta/versions/EditVersionsPanel.java
@@ -257,7 +257,7 @@ public class EditVersionsPanel extends JPanel implements EditVersionsView {
             messagePanel.setMessage("Please select version(s) to mark as final.");
 
         if (versions.length == 1 && !versions[0].isFinalVersion()) {
-            String msg = "Would you like to make version " + versions[0].getId() + 
+            String msg = "Would you like to make version " + versions[0].getVersion() + 
                 " the default version for the dataset?";
             int makeDefault = JOptionPane.showConfirmDialog(parentConsole, msg, "Make Default Version",
                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null);
