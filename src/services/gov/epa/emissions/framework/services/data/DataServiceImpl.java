e9460d96b97071606ff809759572dbd215b864c2
dyang02 <dyang02>
2009-04-01 15:58:29 +0000

    record what is changed on "number of record" on versions table.

--- src/services/gov/epa/emissions/framework/services/data/DataServiceImpl.java
+++ src/services/gov/epa/emissions/framework/services/data/DataServiceImpl.java
@@ -439,30 +439,6 @@ public class DataServiceImpl implements DataService {
         }
         return recordCount;
     }
-    
-    private synchronized int getVersionRecordCount(Version version, String table) throws EmfException {
-        DbServer dbServer = dbServerFactory.getDbServer();
-        int recordCount = 0;
-        String query = "select count(1) as record_count from " + table + " WHERE version IN ("
-        + version.getVersion() + ")";
-        try {
-            if (DebugLevels.DEBUG_17) {
-                LOG.warn("Versioned table data query: " + query);
-            }
-            ResultSet rs = dbServer.getEmissionsDatasource().query().executeQuery(query);
-            if (rs.next())
-                recordCount = rs.getInt(1);
-        } catch (RuntimeException e) {
-            LOG.error("Could not retrieve table record count: " + table, e);
-            throw new EmfException("Could not retrieve table record count: " + table);
-        } catch (SQLException e) {
-            LOG.error("Could not retrieve table record count: " + table, e);
-            throw new EmfException("Could not retrieve table record count: " + table);
-        } finally {
-            closeDB(dbServer);
-        }
-        return recordCount;
-    }
 
     public synchronized void appendData(User user, int srcDSid, int srcDSVersion, String filter, int targetDSid,
             int targetDSVersion, DoubleValue targetStartLineNumber) throws EmfException {
@@ -508,7 +484,7 @@ public class DataServiceImpl implements DataService {
             Version lockedVersion = obtainedLockOnVersion(user, tarVersion.getId());
             
             lockedVersion.setLastModifiedDate(new Date());
-            int num = getVersionRecordCount(lockedVersion, datasource.getName() + "." + targetSources[0].getTable());
+            int num = getNumOfRecords(datasource.getName() + "." + targetSources[0].getTable(), lockedVersion, null);
             lockedVersion.setNumberRecords(num);
             updateVersionNReleaseLock(lockedVersion);
             
