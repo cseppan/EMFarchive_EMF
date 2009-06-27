279ad08f93f3beb5a9e731c0ef3c5e633f868b52
ddelvecchio <ddelvecchio>
2009-06-27 21:19:25 +0000

    new run_sum function that wasn't added in the past

---
+++ deploy/db/cost/functions/run_sum.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION public.run_sum(numeric, text, text)
+  RETURNS numeric AS
+$BODY$
+   if {![info exists GD(sum.$2.$3)]} {
+       set GD(sum.$2.$3) 0.00
+   }
+   if {[argisnull 1]} {
+       return $GD(sum.$2.$3)
+   } else {
+       return [set GD(sum.$2.$3) [expr $GD(sum.$2.$3) + $1]]
+   }
+$BODY$
+  LANGUAGE 'pltcl' VOLATILE
+  COST 100;
+ALTER FUNCTION public.run_sum(numeric, text, text) OWNER TO emf;
+
+
+CREATE OR REPLACE FUNCTION public.run_sum(numeric, text)
+  RETURNS numeric AS
+'select run_sum($1,$2,statement_timestamp()::text)'
+  LANGUAGE 'sql' IMMUTABLE STRICT
+  COST 100;
+ALTER FUNCTION public.run_sum(numeric, text) OWNER TO emf;
+
