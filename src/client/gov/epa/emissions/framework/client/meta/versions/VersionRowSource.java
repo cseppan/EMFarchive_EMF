17d5bb42272535ff28023e5aae13f993542915c0
rross67 <rross67>
2009-11-06 18:51:28 +0000

    Changed "Creator" column to use User.getName() instead of User.getUsername(), as per AE's request

--- src/client/gov/epa/emissions/framework/client/meta/versions/VersionRowSource.java
+++ src/client/gov/epa/emissions/framework/client/meta/versions/VersionRowSource.java
@@ -19,7 +19,7 @@ public class VersionRowSource implements RowSource {
 
     public Object[] values() {
         return new Object[] { selected, source.getName(), new Integer(source.getVersion()),
-                new Long(source.getBase()), source.getCreator().getUsername(),
+                new Long(source.getBase()), source.getCreator().getName(),
                 Boolean.valueOf(source.isFinalVersion()), source.getIntendedUse(), source.getNumberRecords(),
                 format(source.getLastModifiedDate()), source.getDescription() };
     }
