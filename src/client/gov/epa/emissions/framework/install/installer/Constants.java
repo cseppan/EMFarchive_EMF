3e6109a997df97b7fcb379715d55e5da6c6f4aee
rross67 <rross67>
2010-03-30 13:54:52 +0000

    Changed Constants slashes

--- src/client/gov/epa/emissions/framework/install/installer/Constants.java
+++ src/client/gov/epa/emissions/framework/install/installer/Constants.java
@@ -16,18 +16,18 @@ public class Constants {
 	public static final String FILE_LIST = "files.txt";
 	public static final String UPDATE_FILE = "update.dat";
     public static final String EMF_BATCH_FILE = "EMFClient.bat";
-    public static final String CLIENT_JAR_FILE = WORK_DIR + "\\deploy\\client\\emf-client.jar";
-    public static final String REFERENCE_PATH = WORK_DIR + "\\config\\ref\\delimited";
-    public static final String PREFERENCE_PATH = WORK_DIR + "\\config\\preferences";
-    public static final String SCC_FILE = REFERENCE_PATH + "\\scc.txt";
+    public static final String CLIENT_JAR_FILE = WORK_DIR + "/deploy/client/emf-client.jar";
+    public static final String REFERENCE_PATH = WORK_DIR + "/config/ref/delimited";
+    public static final String PREFERENCE_PATH = WORK_DIR + "/config/preferences";
+    public static final String SCC_FILE = REFERENCE_PATH + "/scc.txt";
     public static final String EMF_ICON = "/config/preferences/logo.ico";
     
-    public static final String REMOTE_INPUT_DIR = "T:\\emf_data\\input";
-    public static final String REMOTE_OUTPUT_DIR = "T:\\emf_data\\output";
+    public static final String REMOTE_INPUT_DIR = "T:/emf_data/input";
+    public static final String REMOTE_OUTPUT_DIR = "T:/emf_data/output";
     public static final String JAVA_HOME = System.getProperty("java.home");
-    public static final String INSTALL_HOME = "C:\\Program Files\\EMFClient";
-    public static final String UNINSTALL_DESKTOP = USER_HOME + "\\Desktop\\EMF Client Installer.lnk";
-    public static final String UNINSTALL_START = USER_HOME + "\\Start Menu\\Programs\\EMF Cliet Installer";
+    public static final String INSTALL_HOME = "C:/Program Files/EMFClient";
+    public static final String UNINSTALL_DESKTOP = USER_HOME + "/Desktop/EMF Client Installer.lnk";
+    public static final String UNINSTALL_START = USER_HOME + "/Start Menu/Programs/EMF Cliet Installer";
 	
     // TBD: finish removing these 
 	public static final String[] BAT_FILES = {"runmims"};
