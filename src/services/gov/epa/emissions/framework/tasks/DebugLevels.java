2f8cbf293871f974498622b63a9741559b7f1908
dyang02 <dyang02>
2011-02-16 22:46:59 +0000

    make dataset importers show proper error message

--- src/services/gov/epa/emissions/framework/tasks/DebugLevels.java
+++ src/services/gov/epa/emissions/framework/tasks/DebugLevels.java
@@ -2,7 +2,7 @@ package gov.epa.emissions.framework.tasks;
 
 public class DebugLevels {
 
-    public static final boolean DEBUG_0 = false;
+    public static final boolean DEBUG_0 = true;
 	public static final boolean DEBUG_1 = false;
 	public static final boolean DEBUG_2 = false;
 	public static final boolean DEBUG_3 = false;
