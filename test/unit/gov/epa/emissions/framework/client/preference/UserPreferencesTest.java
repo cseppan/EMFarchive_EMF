6d462f982bad550ef7720cbdb181acc71f2f8d5a
ddelvecchio <ddelvecchio>
2012-05-18 19:55:27 +0000

    Bug 3635
    Changed Properties/Pref manager to allow us to save prefs in the Pref file

--- test/unit/gov/epa/emissions/framework/client/preference/UserPreferencesTest.java
+++ test/unit/gov/epa/emissions/framework/client/preference/UserPreferencesTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 public class UserPreferencesTest extends TestCase {
 
     public void testFetchLocalInputFolder() {
-        Properties props = new Properties();
+        CommentedProperties props = new CommentedProperties();
         props.put("local.input.drive", "d:\\");
         props.put("default.input.folder", "emf_input");
 
@@ -17,7 +17,7 @@ public class UserPreferencesTest extends TestCase {
     }
 
     public void testFetchLocalOutputFolder() {
-        Properties props = new Properties();
+        CommentedProperties props = new CommentedProperties();
         props.put("local.output.drive", "d:\\");
         props.put("default.output.folder", "emf_output");
 
