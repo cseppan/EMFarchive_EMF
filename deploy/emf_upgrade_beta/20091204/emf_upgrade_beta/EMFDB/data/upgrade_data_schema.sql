056f6aaf0bee4cce5cdeaab1db5b05d91316098b
ddelvecchio <ddelvecchio>
2012-09-06 17:14:14 +0000

    bug 3504
    got rid of where versions schema was changed throughout the other table ddls, we'll let the alter table emissions.versions set schema emf handle all the changes in the schema upgrade file

--- deploy/emf_upgrade_beta/20091204/emf_upgrade_beta/EMFDB/data/upgrade_data_schema.sql
+++ deploy/emf_upgrade_beta/20091204/emf_upgrade_beta/EMFDB/data/upgrade_data_schema.sql
@@ -3,10 +3,10 @@ ALTER TABLE emf.datasets_keywords ADD COLUMN kwname varchar(255) NOT NULL DEFAUL
 UPDATE emf.datasets_keywords AS kw SET kwname = (SELECT name FROM emf.keywords WHERE id = kw.keyword_id);
 
 --added (10/7/09) to add descriptions to dataset versions
-ALTER TABLE emf.versions ADD COLUMN description text NOT NULL DEFAULT '';
+ALTER TABLE emissions.versions ADD COLUMN description text NOT NULL DEFAULT '';
 
 --added (10/13/09) to add intended use to dataset versions
-ALTER TABLE emf.versions ADD COLUMN intended_use int4 REFERENCES emf.intended_uses (id) DEFAULT 1;
+ALTER TABLE emissions.versions ADD COLUMN intended_use int4 REFERENCES emf.intended_uses (id) DEFAULT 1;
 
 --added (10/16/09) for use with control measures
 CREATE TABLE "references"
