346b4285dabf102fd40e4a343d8265d1b983eecf
ddelvecchio <ddelvecchio>
2012-09-06 19:48:48 +0000

    For EMF Depolyment 20120906

--- src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
+++ src/client/gov/epa/emissions/framework/client/login/LoginWindow.java
@@ -47,7 +47,7 @@ public class LoginWindow extends EmfFrame implements LoginView {
 
     private ServiceLocator serviceLocator;
 
-    public final static String EMF_VERSION = "v2.5 - 06/27/2012";
+    public final static String EMF_VERSION = "v2.5 - 09/06/2012";
 
     public LoginWindow(ServiceLocator serviceLocator) {
         super("Login", "Login to the Emissions Modeling Framework [" + EMF_VERSION + "]");
